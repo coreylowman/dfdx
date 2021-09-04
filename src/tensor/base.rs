bfc0d3e5a7cd391de6572843fd2045689426e034
Corey Lowman <clowman1993@gmail.com>
2021-09-04 13:13:03 -0400

    Reorganizing tensor impls

---
+++ src/tensor/base.rs
@@ -0,0 +1,110 @@
+use crate::{gradients::*, traits::Tensor};
+use ndarray::prelude::*;
+
+#[derive(Debug)]
+pub struct Tensor0D {
+    pub(super) data: Array0<f32>,
+    pub(super) grad: Option<Grad>,
+}
+
+impl Default for Tensor0D {
+    fn default() -> Self {
+        Self {
+            data: Array0::zeros(()),
+            grad: None,
+        }
+    }
+}
+
+impl Tensor for Tensor0D {
+    const SHAPE: &'static [usize] = &[];
+    type Dimension = Ix0;
+
+    fn grad(&self) -> &Option<Grad> {
+        &self.grad
+    }
+
+    fn mut_grad(&mut self) -> &mut Option<Grad> {
+        &mut self.grad
+    }
+
+    fn data(&self) -> &Array<f32, Self::Dimension> {
+        &self.data
+    }
+
+    fn mut_data(&mut self) -> &mut Array<f32, Self::Dimension> {
+        &mut self.data
+    }
+}
+
+#[derive(Debug)]
+pub struct Tensor1D<const N: usize> {
+    pub(super) data: Array1<f32>,
+    pub(super) grad: Option<Grad>,
+}
+
+impl<const N: usize> Default for Tensor1D<N> {
+    fn default() -> Self {
+        Self {
+            data: Array1::zeros((N,)),
+            grad: None,
+        }
+    }
+}
+
+impl<const N: usize> Tensor for Tensor1D<N> {
+    type Dimension = Ix1;
+    const SHAPE: &'static [usize] = &[N];
+
+    fn grad(&self) -> &Option<Grad> {
+        &self.grad
+    }
+
+    fn mut_grad(&mut self) -> &mut Option<Grad> {
+        &mut self.grad
+    }
+
+    fn data(&self) -> &Array<f32, Self::Dimension> {
+        &self.data
+    }
+
+    fn mut_data(&mut self) -> &mut Array<f32, Self::Dimension> {
+        &mut self.data
+    }
+}
+
+#[derive(Debug)]
+pub struct Tensor2D<const M: usize, const N: usize> {
+    pub(super) data: Array2<f32>,
+    pub(super) grad: Option<Grad>,
+}
+
+impl<const M: usize, const N: usize> Default for Tensor2D<M, N> {
+    fn default() -> Self {
+        Self {
+            data: Array2::zeros((M, N)),
+            grad: Default::default(),
+        }
+    }
+}
+
+impl<const M: usize, const N: usize> Tensor for Tensor2D<M, N> {
+    type Dimension = Ix2;
+    const SHAPE: &'static [usize] = &[M, N];
+
+    fn grad(&self) -> &Option<Grad> {
+        &self.grad
+    }
+
+    fn mut_grad(&mut self) -> &mut Option<Grad> {
+        &mut self.grad
+    }
+
+    fn data(&self) -> &Array<f32, Self::Dimension> {
+        &self.data
+    }
+
+    fn mut_data(&mut self) -> &mut Array<f32, Self::Dimension> {
+        &mut self.data
+    }
+}
--- src/tensor/ops.rs
+++ src/tensor/ops.rs
@@ -1,81 +1,10 @@
+use super::base::*;
 use crate::{
     gradients::*,
     traits::{Params, Tensor},
 };
 use ndarray::prelude::*;
 use std::ops::{Add, Mul, Sub};
-
-#[derive(Debug)]
-pub struct Tensor0D {
-    data: Array0<f32>,
-    grad: Option<Grad>,
-}
-
-impl Default for Tensor0D {
-    fn default() -> Self {
-        Self {
-            data: Array0::zeros(()),
-            grad: None,
-        }
-    }
-}
-
-impl Tensor for Tensor0D {
-    const SHAPE: &'static [usize] = &[];
-    type Dimension = Ix0;
-
-    fn grad(&self) -> &Option<Grad> {
-        &self.grad
-    }
-
-    fn mut_grad(&mut self) -> &mut Option<Grad> {
-        &mut self.grad
-    }
-
-    fn data(&self) -> &Array<f32, Self::Dimension> {
-        &self.data
-    }
-
-    fn mut_data(&mut self) -> &mut Array<f32, Self::Dimension> {
-        &mut self.data
-    }
-}
-
-#[derive(Debug)]
-pub struct Tensor1D<const N: usize> {
-    data: Array1<f32>,
-    grad: Option<Grad>,
-}
-
-impl<const N: usize> Default for Tensor1D<N> {
-    fn default() -> Self {
-        Self {
-            data: Array1::zeros((N,)),
-            grad: None,
-        }
-    }
-}
-
-impl<const N: usize> Tensor for Tensor1D<N> {
-    type Dimension = Ix1;
-    const SHAPE: &'static [usize] = &[N];
-
-    fn grad(&self) -> &Option<Grad> {
-        &self.grad
-    }
-
-    fn mut_grad(&mut self) -> &mut Option<Grad> {
-        &mut self.grad
-    }
-
-    fn data(&self) -> &Array<f32, Self::Dimension> {
-        &self.data
-    }
-
-    fn mut_data(&mut self) -> &mut Array<f32, Self::Dimension> {
-        &mut self.data
-    }
-}
 
 impl<const N: usize> Add for &mut Tensor1D<N> {
     type Output = Tensor1D<N>;
@@ -178,42 +107,6 @@ impl<const N: usize> Tensor1D<N> {
             data: arr0(self.data.mean().unwrap()),
             grad,
         }
-    }
-}
-
-#[derive(Debug)]
-pub struct Tensor2D<const M: usize, const N: usize> {
-    data: Array2<f32>,
-    grad: Option<Grad>,
-}
-
-impl<const M: usize, const N: usize> Default for Tensor2D<M, N> {
-    fn default() -> Self {
-        Self {
-            data: Array2::zeros((M, N)),
-            grad: Default::default(),
-        }
-    }
-}
-
-impl<const M: usize, const N: usize> Tensor for Tensor2D<M, N> {
-    type Dimension = Ix2;
-    const SHAPE: &'static [usize] = &[M, N];
-
-    fn grad(&self) -> &Option<Grad> {
-        &self.grad
-    }
-
-    fn mut_grad(&mut self) -> &mut Option<Grad> {
-        &mut self.grad
-    }
-
-    fn data(&self) -> &Array<f32, Self::Dimension> {
-        &self.data
-    }
-
-    fn mut_data(&mut self) -> &mut Array<f32, Self::Dimension> {
-        &mut self.data
     }
 }
 
