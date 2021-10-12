56abde8adae1dfcc516043f7f2d034e39fc51870
Corey Lowman <clowman1993@gmail.com>
2021-10-11 22:55:31 -0400

    Adding ModuleChain example

---
+++ examples/chain.rs
@@ -0,0 +1,20 @@
+use ndarray_rand::rand::prelude::*;
+use stag::nn::{Linear, ModuleChain, ReLU};
+use stag::prelude::*;
+
+fn main() {
+    let mut rng = StdRng::seed_from_u64(0);
+
+    // initialize the MLP
+    let mut model: ModuleChain<Linear<4, 2>, ReLU<Tensor1D<2>>> = Default::default();
+    model.init(&mut rng);
+
+    // create a 1x10 tensor with zeros
+    let mut x: Tensor2D<1, 4> = Default::default();
+
+    // forward through the model
+    let y = model.forward(&mut x);
+
+    println!("{:#}", y.data());
+    // [[0.741256, 0]]
+}
