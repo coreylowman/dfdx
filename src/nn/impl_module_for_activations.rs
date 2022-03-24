447ad6ab9f14bd29e5091a6d7f0f8a6eccf7cd37
Corey Lowman <clowman1993@gmail.com>
2022-03-24 08:16:52 -0400

    Moving module impl for activations to macro on actual structs

--- src/nn/impl_module_for_activations.rs
+++ src/nn/impl_module_for_activations.rs
@@ -2,17 +2,31 @@ use super::traits::Module;
 use crate::prelude::*;
 use rand::{distributions::Distribution, Rng};
 
-impl<F: DifferentiableFunction> CanUpdateWithTape for F {
-    fn update_with_tape(&mut self, _: &GradientTape) {}
+macro_rules! activation_impls {
+    ($typename:ty) => {
+        impl CanUpdateWithTape for $typename {
+            fn update_with_tape(&mut self, _: &GradientTape) {}
+        }
+
+        impl Randomize for $typename {
+            fn randomize<R: Rng, D: Distribution<f32>>(&mut self, _: &mut R, _: &D) {}
+        }
+
+        impl<T: Tensor> Module<T> for $typename {
+            type Output = T;
+            fn forward(&self, input: T) -> Self::Output {
+                apply::<T, Self>(input)
+            }
+        }
+    };
 }
 
-impl<F: DifferentiableFunction> Randomize for F {
-    fn randomize<R: Rng, D: Distribution<f32>>(&mut self, _: &mut R, _: &D) {}
-}
-
-impl<F: DifferentiableFunction + Default, T: Tensor> Module<T> for F {
-    type Output = T;
-    fn forward(&self, input: T) -> Self::Output {
-        apply::<T, Self>(input)
-    }
-}
+activation_impls!(ReLU);
+activation_impls!(Sin);
+activation_impls!(Cos);
+activation_impls!(Ln);
+activation_impls!(Exp);
+activation_impls!(Sigmoid);
+activation_impls!(Tanh);
+activation_impls!(Square);
+activation_impls!(Abs);
