f6d768f9f4b63266f1af4dfbc872d9670c4536b8
Corey Lowman <clowman1993@gmail.com>
2022-04-21 08:27:44 -0400

    Adding activation with ref functions

--- src/tensor_ops/impl_unary_ops.rs
+++ src/tensor_ops/impl_unary_ops.rs
@@ -48,7 +48,7 @@ pub fn apply<T: Tensor, F: DifferentiableFunction>(t: T) -> T {
 }
 
 macro_rules! apply_impl {
-    ($trait_name:tt, $method_name:tt, $activation_struct:ty) => {
+    ($trait_name:ident, $method_name:ident, $activation_struct:ident) => {
         pub trait $trait_name {
             fn $method_name(self) -> Self;
         }
@@ -70,3 +70,34 @@ apply_impl!(HasSigmoidMethod, sigmoid, Sigmoid);
 apply_impl!(HasTanhMethod, tanh, Tanh);
 apply_impl!(HasSquareMethod, square, Square);
 apply_impl!(HasAbsMethod, abs, Abs);
+
+pub fn apply_ref<T, F: DifferentiableFunction>(t: &T) -> T
+where
+    T: Tensor<TapeHolder = NoTape> + TensorCreator,
+{
+    T::new(t.data().mapv(F::f))
+}
+
+macro_rules! apply_ref_impl {
+    ($trait_name:ident, $method_name:ident, $activation_struct:ident) => {
+        pub trait $trait_name {
+            fn $method_name(&self) -> Self;
+        }
+
+        impl<T: Tensor<TapeHolder = NoTape> + TensorCreator> $trait_name for T {
+            fn $method_name(&self) -> Self {
+                apply_ref::<Self, $activation_struct>(self)
+            }
+        }
+    };
+}
+
+apply_ref_impl!(HasReLURefMethod, relu_, ReLU);
+apply_ref_impl!(HasSinRefMethod, sin_, Sin);
+apply_ref_impl!(HasCosRefMethod, cos_, Cos);
+apply_ref_impl!(HasLnRefMethod, ln_, Ln);
+apply_ref_impl!(HasExpRefMethod, exp_, Exp);
+apply_ref_impl!(HasSigmoidRefMethod, sigmoid_, Sigmoid);
+apply_ref_impl!(HasTanhRefMethod, tanh_, Tanh);
+apply_ref_impl!(HasSquareRefMethod, square_, Square);
+apply_ref_impl!(HasAbsRefMethod, abs_, Abs);
