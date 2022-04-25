8af357893724d7b1e98e8a21697ddf86bc213626
Corey Lowman <clowman1993@gmail.com>
2022-04-25 11:48:09 -0400

    Testing nans_to

--- src/tensor_ops/impl_nans.rs
+++ src/tensor_ops/impl_nans.rs
@@ -11,14 +11,10 @@ fn nans_to<T: Tensor>(t: T, value: f32) -> T {
     result.with_tape_holder(tape_holder)
 }
 
-pub trait CanReplaceNans {
-    fn nans_to(self, value: f32) -> Self;
-}
-
 macro_rules! tensor_impl {
     ($typename:ident, [$($Vs:tt),*]) => {
-impl<$(const $Vs: usize, )* H: TapeHolder> CanReplaceNans for $typename<$($Vs, )* H> {
-    fn nans_to(self, value: f32) -> Self {
+impl<$(const $Vs: usize, )* H: TapeHolder> $typename<$($Vs, )* H> {
+    pub fn nans_to(self, value: f32) -> Self {
         nans_to(self, value)
     }
 }
@@ -33,13 +29,43 @@ tensor_impl!(Tensor4D, [M, N, O, P]);
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+    use ndarray::prelude::*;
+
+    #[test]
+    fn test_nans_0d() {
+        let t = Tensor0D::new(arr0(f32::NAN));
+        let r = t.with_tape().nans_to(0.0);
+        assert_eq!(r.data(), arr0(0.0));
+        let gradients = backward(r.mean());
+        assert_eq!(
+            gradients.gradient_for(t.id()).to_shape(t.shape()).unwrap(),
+            arr0(0.0)
+        );
+    }
+
     #[test]
     fn test_nans_1d() {
-        todo!();
+        let t: Tensor1D<4> = Tensor1D::new(arr1(&[1.0, f32::NAN, f32::NAN, 4.0]));
+        let r = t.with_tape().nans_to(0.0);
+        assert_eq!(r.data(), arr1(&[1.0, 0.0, 0.0, 4.0]));
+        let gradients = backward(r.mean());
+        assert_eq!(
+            gradients.gradient_for(t.id()).to_shape(t.shape()).unwrap(),
+            arr1(&[0.25, 0.0, 0.0, 0.25])
+        );
     }
 
     #[test]
     fn test_nans_2d() {
-        todo!()
+        let t: Tensor2D<2, 3> =
+            Tensor2D::new(arr2(&[[1.0, f32::NAN, 3.0], [f32::NAN, 4.0, f32::NAN]]));
+        let r = t.with_tape().nans_to(0.0);
+        assert_eq!(r.data(), arr2(&[[1.0, 0.0, 3.0], [0.0, 4.0, 0.0]]));
+        let gradients = backward(r.mean());
+        assert_eq!(
+            gradients.gradient_for(t.id()).to_shape(t.shape()).unwrap(),
+            arr2(&[[1.0 / 6.0, 0.0, 1.0 / 6.0], [0.0, 1.0 / 6.0, 0.0]])
+        );
     }
 }
