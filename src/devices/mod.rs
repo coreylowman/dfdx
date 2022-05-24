023f5c45108d44a7d75d62e6b6015d869229a649
Corey Lowman <clowman1993@gmail.com>
2022-05-24 13:39:53 -0400

    Removing map_inner, Changing reduce_inner to reduce_last_dim

--- src/devices/mod.rs
+++ src/devices/mod.rs
@@ -2,9 +2,8 @@
 
 mod fill;
 mod map;
-mod map_inner;
 mod reduce;
-mod reduce_inner;
+mod reduce_last_dim;
 mod zero;
 mod zip_map;
 
@@ -13,20 +12,30 @@ pub struct Cpu;
 
 pub use fill::*;
 pub use map::*;
-pub use map_inner::*;
 pub use reduce::*;
-pub use reduce_inner::*;
+pub use reduce_last_dim::*;
 pub use zero::*;
 pub use zip_map::*;
 
 /// Represents something that can act on `T`.
 pub trait Device<T: crate::arrays::CountElements>:
-    FillElements<T> + ZipMapElements<T, T> + MapElements<T> + ReduceElements<T> + AllocateZeros
+    FillElements<T>
+    + ZipMapElements<T, T>
+    + MapElements<T>
+    + ReduceElements<T>
+    + AllocateZeros
+    + ReduceLastDim<T>
+    + ZipMapElements<T, <Self as ReduceLastDim<T>>::Reduced>
 {
 }
 
 impl Device<f32> for Cpu {}
-impl<T: crate::arrays::CountElements, const M: usize> Device<[T; M]> for Cpu where Cpu: Device<T> {}
+impl<T: crate::arrays::CountElements, const M: usize> Device<[T; M]> for Cpu where
+    Cpu: Device<T>
+        + ReduceLastDim<[T; M]>
+        + ZipMapElements<[T; M], <Self as ReduceLastDim<[T; M]>>::Reduced>
+{
+}
 
 /// A [HasArrayType] that has a [Device] for its [HasArrayType::Array]
 pub trait HasDevice: crate::arrays::HasArrayType {
---
+++ src/devices/reduce_last_dim.rs
@@ -0,0 +1,131 @@
+use super::{AllocateZeros, Cpu};
+use crate::arrays::CountElements;
+
+/// Something that can have its last dimension (inner most dimension) reduced to 1 number.
+///
+/// Note: This currently cannot be implemented with recursive traits because we need
+/// to support reduce `[f32; M]` to `f32` AND `f32` -> `f32`. This means it is also valid
+/// to reduce `[f32; M]` to `[f32; M]` if recursive traits were used, which doesn't make sense.
+pub trait ReduceLastDim<T: CountElements>: AllocateZeros {
+    type Reduced: CountElements<Dtype = T::Dtype>;
+
+    fn reduce_last_dim_into<F: FnMut(T::Dtype, T::Dtype) -> T::Dtype + Copy>(
+        inp: &T,
+        out: &mut Self::Reduced,
+        f: F,
+    );
+
+    fn reduce_last_dim<F: FnMut(T::Dtype, T::Dtype) -> T::Dtype + Copy>(
+        inp: &T,
+        f: F,
+    ) -> Box<Self::Reduced> {
+        let mut out = Self::zeros();
+        Self::reduce_last_dim_into(inp, &mut out, f);
+        out
+    }
+}
+
+impl ReduceLastDim<f32> for Cpu {
+    type Reduced = f32;
+    fn reduce_last_dim_into<F: FnMut(f32, f32) -> f32 + Copy>(
+        inp: &f32,
+        out: &mut Self::Reduced,
+        _f: F,
+    ) {
+        *out = *inp;
+    }
+}
+
+impl<const M: usize> ReduceLastDim<[f32; M]> for Cpu {
+    type Reduced = f32;
+    fn reduce_last_dim_into<F: FnMut(f32, f32) -> f32 + Copy>(
+        inp: &[f32; M],
+        out: &mut Self::Reduced,
+        f: F,
+    ) {
+        *out = inp.iter().cloned().reduce(f).unwrap();
+    }
+}
+
+impl<const M: usize, const N: usize> ReduceLastDim<[[f32; N]; M]> for Cpu {
+    type Reduced = [f32; M];
+    fn reduce_last_dim_into<F: FnMut(f32, f32) -> f32 + Copy>(
+        inp: &[[f32; N]; M],
+        out: &mut Self::Reduced,
+        f: F,
+    ) {
+        for i in 0..M {
+            Self::reduce_last_dim_into(&inp[i], &mut out[i], f);
+        }
+    }
+}
+
+impl<const M: usize, const N: usize, const O: usize> ReduceLastDim<[[[f32; O]; N]; M]> for Cpu {
+    type Reduced = [[f32; N]; M];
+    fn reduce_last_dim_into<F: FnMut(f32, f32) -> f32 + Copy>(
+        inp: &[[[f32; O]; N]; M],
+        out: &mut Self::Reduced,
+        f: F,
+    ) {
+        for i in 0..M {
+            Self::reduce_last_dim_into(&inp[i], &mut out[i], f);
+        }
+    }
+}
+
+impl<const M: usize, const N: usize, const O: usize, const P: usize>
+    ReduceLastDim<[[[[f32; P]; O]; N]; M]> for Cpu
+{
+    type Reduced = [[[f32; O]; N]; M];
+    fn reduce_last_dim_into<F: FnMut(f32, f32) -> f32 + Copy>(
+        inp: &[[[[f32; P]; O]; N]; M],
+        out: &mut Self::Reduced,
+        f: F,
+    ) {
+        for i in 0..M {
+            Self::reduce_last_dim_into(&inp[i], &mut out[i], f);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::prelude::ZeroElements;
+
+    use super::*;
+
+    #[test]
+    fn test_reduce_inner_0d() {
+        let t = 3.14;
+        let mut out = ZeroElements::ZEROS;
+        Cpu::reduce_last_dim_into(&t, &mut out, f32::max);
+        assert_eq!(out, 3.14);
+    }
+
+    #[test]
+    fn test_reduce_inner_1d() {
+        let t = [1.0, 2.0, 3.0];
+        let mut out: f32 = ZeroElements::ZEROS;
+        Cpu::reduce_last_dim_into(&t, &mut out, f32::max);
+        assert_eq!(out, 3.0);
+    }
+
+    #[test]
+    fn test_reduce_inner_2d() {
+        let t = [[1.0, 2.0, 3.0], [6.0, 5.0, 4.0]];
+        let mut out: [f32; 2] = ZeroElements::ZEROS;
+        Cpu::reduce_last_dim_into(&t, &mut out, f32::max);
+        assert_eq!(out, [3.0, 6.0]);
+    }
+
+    #[test]
+    fn test_reduce_inner_3d() {
+        let t = [
+            [[1.0, 2.0, 3.0], [6.0, 5.0, 4.0]],
+            [[-1.0, -2.0, -3.0], [-6.0, -5.0, -4.0]],
+        ];
+        let mut out: [[f32; 2]; 2] = ZeroElements::ZEROS;
+        Cpu::reduce_last_dim_into(&t, &mut out, f32::max);
+        assert_eq!(out, [[3.0, 6.0], [-1.0, -4.0]]);
+    }
+}
