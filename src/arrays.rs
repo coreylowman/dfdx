bba249354f7793a237b59c5329766b618cb69581
Corey Lowman <clowman1993@gmail.com>
2022-06-25 12:49:32 -0400

    Adding ref_first_elem and mut_first_elem to CountElements

--- src/arrays.rs
+++ src/arrays.rs
@@ -2,24 +2,51 @@
 
 /// Represents something with a compile time known number of elements
 pub trait CountElements: Clone {
-    const NUM_ELEMENTS: usize;
     type Dtype: Clone + Default;
+    const NUM_ELEMENTS: usize;
     const NUM_BYTES: usize = Self::NUM_ELEMENTS * std::mem::size_of::<Self::Dtype>();
+
+    fn ref_first_elem(&self) -> &Self::Dtype;
+    fn mut_first_elem(&mut self) -> &mut Self::Dtype;
 }
 
 impl CountElements for f32 {
-    const NUM_ELEMENTS: usize = 1;
     type Dtype = Self;
+    const NUM_ELEMENTS: usize = 1;
+
+    fn ref_first_elem(&self) -> &Self::Dtype {
+        self
+    }
+
+    fn mut_first_elem(&mut self) -> &mut Self::Dtype {
+        self
+    }
 }
 
 impl CountElements for usize {
-    const NUM_ELEMENTS: usize = 1;
     type Dtype = Self;
+    const NUM_ELEMENTS: usize = 1;
+
+    fn ref_first_elem(&self) -> &Self::Dtype {
+        self
+    }
+
+    fn mut_first_elem(&mut self) -> &mut Self::Dtype {
+        self
+    }
 }
 
 impl<T: CountElements, const M: usize> CountElements for [T; M] {
-    const NUM_ELEMENTS: usize = M * T::NUM_ELEMENTS;
     type Dtype = T::Dtype;
+    const NUM_ELEMENTS: usize = M * T::NUM_ELEMENTS;
+
+    fn ref_first_elem(&self) -> &Self::Dtype {
+        self[0].ref_first_elem()
+    }
+
+    fn mut_first_elem(&mut self) -> &mut Self::Dtype {
+        self[0].mut_first_elem()
+    }
 }
 
 /// Something that has compile time known zero values.
@@ -63,5 +90,12 @@ mod tests {
     #[test]
     fn test_3d_count() {
         assert_eq!(30, <[[[f32; 2]; 3]; 5]>::NUM_ELEMENTS);
+    }
+
+    #[test]
+    fn test_first_elem_ref() {
+        let mut a: [[f32; 2]; 3] = [[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]];
+        assert_eq!(a.ref_first_elem(), &1.0);
+        assert_eq!(a.mut_first_elem(), &mut 1.0);
     }
 }
