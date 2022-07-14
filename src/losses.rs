ecd602ffb9e3d8fc6518e57cf7e05e4a28f2fb4a
Corey Lowman <clowman1993@gmail.com>
2022-07-14 17:13:26 -0400

    #79 Adding smooth_l1_loss and huber_loss

--- src/losses.rs
+++ src/losses.rs
@@ -26,6 +26,77 @@ pub fn mae_loss<T: Tensor<Dtype = f32>>(pred: T, targ: &T::NoTape) -> Tensor0D<T
     mean(abs(sub(pred, targ)))
 }
 
+/// [Huber Loss](https://en.wikipedia.org/wiki/Huber_loss)
+/// uses absolute error when the error is higher than `beta`, and squared error when the
+/// error is lower than `beta`.
+///
+/// It computes:
+/// 1. if `|x - y| < delta`: `0.5 * (x - y)^2`
+/// 2. otherwise: `delta * (|x - y| - 0.5 * delta)`
+///
+/// # Example
+/// ```rust
+/// # use dfdx::prelude::*;
+/// let x = Tensor1D::new([-1.0, -0.5]);
+/// let y = Tensor1D::new([0.5, 0.5]);
+/// let loss = huber_loss(x.traced(), &y, 1.0);
+/// ```
+pub fn huber_loss<T: Tensor<Dtype = f32>>(
+    pred: T,
+    targ: &T::NoTape,
+    delta: T::Dtype,
+) -> Tensor0D<T::Tape> {
+    let f = move |x: &f32, y: &f32| {
+        if (x - y).abs() < delta {
+            (x - y).powi(2) * 0.5
+        } else {
+            (x - y).abs() * delta - 0.5 * delta * delta
+        }
+    };
+    let dfdx = move |x: &f32, y: &f32| {
+        if (x - y) == 0.0 {
+            0.0
+        } else if (x - y).abs() < delta {
+            x - y
+        } else {
+            (x - y).signum() * delta
+        }
+    };
+    let dfdy = move |x: &f32, y: &f32| {
+        if (x - y) == 0.0 {
+            0.0
+        } else if (x - y).abs() < delta {
+            y - x
+        } else {
+            (y - x).signum() * delta
+        }
+    };
+    mean(binary_map::binary_map(pred, targ, f, dfdx, dfdy))
+}
+
+/// Smooth l1 loss (closely related to [Huber Loss](https://en.wikipedia.org/wiki/Huber_loss))
+/// uses absolute error when the error is higher than `beta`, and squared error when the
+/// error is lower than `beta`.
+///
+/// It computes:
+/// 1. if `|x - y| < beta`: `0.5 * (x - y)^2 / beta`
+/// 2. otherwise: `|x - y| - 0.5 * beta`
+///
+/// # Example
+/// ```rust
+/// # use dfdx::prelude::*;
+/// let x = Tensor1D::new([-1.0, -0.5]);
+/// let y = Tensor1D::new([0.5, 0.5]);
+/// let loss = smooth_l1_loss(x.traced(), &y, 1.0);
+/// ```
+pub fn smooth_l1_loss<T: Tensor<Dtype = f32>>(
+    pred: T,
+    targ: &T::NoTape,
+    beta: T::Dtype,
+) -> Tensor0D<T::Tape> {
+    huber_loss(pred, targ, beta) / beta
+}
+
 /// [Cross entropy loss](https://en.wikipedia.org/wiki/Cross_entropy#Cross-entropy_loss_function_and_logistic_regression).
 /// This computes: `-(logits.log_softmax() * target_probs).sum(-1).mean()`
 ///
@@ -264,6 +335,76 @@ mod tests {
                 [-11.111112, -11.111112, -11.111112],
                 [11.111112, 11.111112, 11.111112],
                 [0.11111111, 0.0, -0.11111111]
+            ]
+        );
+    }
+
+    #[test]
+    fn test_huber_loss() {
+        let x = Tensor2D::new([
+            [1.0095837, -1.0026205, -0.1126093, -0.1539351, -0.3688708],
+            [2.6373475, 0.6761999, -1.3586733, 0.486154, -0.6206786],
+            [-1.2967702, -0.1273358, 1.3558478, 0.0787393, 1.0921133],
+        ]);
+        let y = Tensor2D::new([
+            [1.2569424, -1.2246597, 0.7995769, 0.0339246, -0.3688708],
+            [1.472675, 0.8260061, 0.7839395, -0.0541475, -0.6206786],
+            [-2.0449343, 1.8117315, 1.7505344, -1.2522424, 1.0921133],
+        ]);
+
+        let loss = huber_loss(x.trace(), &y, 0.5);
+        assert_eq!(loss.data(), &0.24506615);
+
+        let gradients = loss.backward();
+        assert_eq!(
+            gradients.ref_gradient(&x),
+            &[
+                [-0.016490579, 0.014802615, -0.033333335, -0.012523981, 0.0],
+                [0.033333335, -0.0099870805, -0.033333335, 0.033333335, 0.0],
+                [0.033333335, -0.033333335, -0.02631244, 0.033333335, 0.0]
+            ]
+        );
+        assert_eq!(
+            gradients.ref_gradient(&y),
+            &[
+                [0.016490579, -0.014802615, 0.033333335, 0.012523981, 0.0],
+                [-0.033333335, 0.0099870805, 0.033333335, -0.033333335, 0.0],
+                [-0.033333335, 0.033333335, 0.02631244, -0.033333335, 0.0]
+            ]
+        );
+    }
+
+    #[test]
+    fn test_smooth_l1_loss() {
+        let x = Tensor2D::new([
+            [1.0095837, -1.0026205, -0.1126093, -0.1539351, -0.3688708],
+            [2.6373475, 0.6761999, -1.3586733, 0.486154, -0.6206786],
+            [-1.2967702, -0.1273358, 1.3558478, 0.0787393, 1.0921133],
+        ]);
+        let y = Tensor2D::new([
+            [1.2569424, -1.2246597, 0.7995769, 0.0339246, -0.3688708],
+            [1.472675, 0.8260061, 0.7839395, -0.0541475, -0.6206786],
+            [-2.0449343, 1.8117315, 1.7505344, -1.2522424, 1.0921133],
+        ]);
+
+        let loss = smooth_l1_loss(x.trace(), &y, 0.5);
+        assert_eq!(loss.data(), &0.4901323);
+
+        let gradients = loss.backward();
+        assert_eq!(
+            gradients.ref_gradient(&x),
+            &[
+                [-0.032981157, 0.02960523, -0.06666667, -0.025047962, 0.0],
+                [0.06666667, -0.019974161, -0.06666667, 0.06666667, 0.0],
+                [0.06666667, -0.06666667, -0.05262488, 0.06666667, 0.0]
+            ]
+        );
+        assert_eq!(
+            gradients.ref_gradient(&y),
+            &[
+                [0.032981157, -0.02960523, 0.06666667, 0.025047962, 0.0],
+                [-0.06666667, 0.019974161, 0.06666667, -0.06666667, 0.0],
+                [-0.06666667, 0.06666667, 0.05262488, -0.06666667, 0.0]
             ]
         );
     }
