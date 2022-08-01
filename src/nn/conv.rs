c4cae9f42092a807ac72f567d223139e4a1038bc
Corey Lowman <clowman1993@gmail.com>
2022-08-01 15:21:54 -0400

    Adding tests for Conv2D layer

--- src/nn/conv.rs
+++ src/nn/conv.rs
@@ -160,29 +160,97 @@ where
 #[cfg(test)]
 mod tests {
     use super::*;
+    use rand::{prelude::StdRng, SeedableRng};
+    use std::fs::File;
+    use tempfile::NamedTempFile;
 
     #[test]
-    fn test_forward_3d() {
-        todo!();
+    fn test_forward_3d_sizes() {
+        type Img = Tensor3D<3, 10, 10>;
+        let _: Tensor3D<2, 8, 8> = Conv2D::<3, 2, 3>::default().forward(Img::zeros());
+        let _: Tensor3D<4, 8, 8> = Conv2D::<3, 4, 3>::default().forward(Img::zeros());
+        let _: Tensor3D<4, 9, 9> = Conv2D::<3, 4, 2>::default().forward(Img::zeros());
+        let _: Tensor3D<4, 7, 7> = Conv2D::<3, 4, 4>::default().forward(Img::zeros());
+        let _: Tensor3D<2, 4, 4> = Conv2D::<3, 2, 3, 2>::default().forward(Img::zeros());
+        let _: Tensor3D<2, 3, 3> = Conv2D::<3, 2, 3, 3>::default().forward(Img::zeros());
+        let _: Tensor3D<2, 10, 10> = Conv2D::<3, 2, 3, 1, 1>::default().forward(Img::zeros());
+        let _: Tensor3D<2, 12, 12> = Conv2D::<3, 2, 3, 1, 2>::default().forward(Img::zeros());
+        let _: Tensor3D<2, 6, 6> = Conv2D::<3, 2, 3, 2, 2>::default().forward(Img::zeros());
     }
 
     #[test]
-    fn test_forward_4d() {
-        todo!();
+    fn test_forward_4d_sizes() {
+        type Img = Tensor4D<5, 3, 10, 10>;
+        let _: Tensor4D<5, 2, 8, 8> = Conv2D::<3, 2, 3>::default().forward(Img::zeros());
+        let _: Tensor4D<5, 4, 8, 8> = Conv2D::<3, 4, 3>::default().forward(Img::zeros());
+        let _: Tensor4D<5, 4, 9, 9> = Conv2D::<3, 4, 2>::default().forward(Img::zeros());
+        let _: Tensor4D<5, 4, 7, 7> = Conv2D::<3, 4, 4>::default().forward(Img::zeros());
+        let _: Tensor4D<5, 2, 4, 4> = Conv2D::<3, 2, 3, 2>::default().forward(Img::zeros());
+        let _: Tensor4D<5, 2, 3, 3> = Conv2D::<3, 2, 3, 3>::default().forward(Img::zeros());
+        let _: Tensor4D<5, 2, 10, 10> = Conv2D::<3, 2, 3, 1, 1>::default().forward(Img::zeros());
+        let _: Tensor4D<5, 2, 12, 12> = Conv2D::<3, 2, 3, 1, 2>::default().forward(Img::zeros());
+        let _: Tensor4D<5, 2, 6, 6> = Conv2D::<3, 2, 3, 2, 2>::default().forward(Img::zeros());
     }
 
     #[test]
-    fn test_save() {
-        todo!();
+    fn test_2_conv_sizes() {
+        type A = Conv2D<1, 2, 3>;
+        type B = Conv2D<2, 4, 3>;
+        let _: Tensor3D<4, 6, 6> = B::default().forward(Tensor3D::<2, 8, 8>::zeros());
+        let _: Tensor3D<4, 6, 6> = <(A, B)>::default().forward(Tensor3D::<1, 10, 10>::zeros());
     }
 
     #[test]
-    fn test_load() {
-        todo!();
+    fn test_3_conv_sizes() {
+        type A = Conv2D<1, 2, 3>;
+        type B = Conv2D<2, 4, 3>;
+        type C = Conv2D<4, 1, 1, 1, 1>;
+
+        type Img = Tensor3D<1, 10, 10>;
+        let _: Tensor3D<2, 8, 8> = A::default().forward(Img::zeros());
+        let _: Tensor3D<4, 6, 6> = <(A, B)>::default().forward(Img::zeros());
+        let _: Tensor3D<1, 8, 8> = C::default().forward(Tensor3D::<4, 6, 6>::zeros());
+        let _: Tensor3D<1, 8, 8> = <(A, B, C)>::default().forward(Img::zeros());
     }
 
     #[test]
-    fn test_multi_layered_sizes() {
-        todo!();
+    fn test_save_conv2d() {
+        let model: Conv2D<2, 4, 3> = Default::default();
+        let file = NamedTempFile::new().expect("failed to create tempfile");
+        model
+            .save(file.path().to_str().unwrap())
+            .expect("failed to save model");
+        let f = File::open(file.path()).expect("failed to open resulting file");
+        let mut zip = ZipArchive::new(f).expect("failed to create zip archive from file");
+        {
+            let weight_file = zip
+                .by_name("weight.npy")
+                .expect("failed to find weight.npy file");
+            assert!(weight_file.size() > 0);
+        }
+        {
+            let bias_file = zip
+                .by_name("bias.npy")
+                .expect("failed to find bias.npy file");
+            assert!(bias_file.size() > 0);
+        }
+    }
+
+    #[test]
+    fn test_load_conv() {
+        let mut rng = StdRng::seed_from_u64(0);
+        let mut saved_model: Conv2D<2, 4, 3> = Default::default();
+        saved_model.reset_params(&mut rng);
+
+        let file = NamedTempFile::new().expect("failed to create tempfile");
+        assert!(saved_model.save(file.path().to_str().unwrap()).is_ok());
+
+        let mut loaded_model: Conv2D<2, 4, 3> = Default::default();
+        assert!(loaded_model.weight.data() != saved_model.weight.data());
+        assert!(loaded_model.bias.data() != saved_model.bias.data());
+
+        assert!(loaded_model.load(file.path().to_str().unwrap()).is_ok());
+        assert_eq!(loaded_model.weight.data(), saved_model.weight.data());
+        assert_eq!(loaded_model.bias.data(), saved_model.bias.data());
     }
 }
