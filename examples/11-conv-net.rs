190d9f12c1c2d17ede9015b94086c01fda34f210
Corey Lowman <clowman1993@gmail.com>
2022-10-15 13:31:52 -0400

    Fixing warnings in examples/11-conv-net.rs

--- examples/11-conv-net.rs
+++ examples/11-conv-net.rs
@@ -2,6 +2,7 @@
 //! layers on nightly rust.
 
 #![cfg_attr(feature = "nightly", feature(generic_const_exprs))]
+
 use dfdx::prelude::*;
 use rand::thread_rng;
 
@@ -18,15 +19,14 @@ fn main() {
     let mut m: Model = Default::default();
     m.reset_params(&mut rng);
 
-    // single image forward
-    let x: Tensor3D<3, 28, 28> = TensorCreator::randn(&mut rng);
-
     #[cfg(feature = "nightly")]
-    let _: Tensor1D<10> = m.forward(x);
+    {
+        // single image forward
+        let x: Tensor3D<3, 28, 28> = TensorCreator::randn(&mut rng);
+        let _: Tensor1D<10> = m.forward(x);
 
-    // batched image forward
-    let x: Tensor4D<32, 3, 28, 28> = TensorCreator::randn(&mut rng);
-
-    #[cfg(feature = "nightly")]
-    let _: Tensor2D<32, 10> = m.forward(x);
+        // batched image forward
+        let x: Tensor4D<32, 3, 28, 28> = TensorCreator::randn(&mut rng);
+        let _: Tensor2D<32, 10> = m.forward(x);
+    }
 }
