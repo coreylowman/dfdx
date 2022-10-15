258faaf16304ec9c9b13f1a13ec2e695d40fa176
Corey Lowman <coreylowman@users.noreply.github.com>
2022-10-15 13:29:08 -0400

    Renaming Flatten2D (#243)

--- examples/11-conv-net.rs
+++ examples/11-conv-net.rs
@@ -2,34 +2,31 @@
 //! layers on nightly rust.
 
 #![cfg_attr(feature = "nightly", feature(generic_const_exprs))]
+use dfdx::prelude::*;
+use rand::thread_rng;
 
-#[cfg(not(feature = "nightly"))]
+type Model = (
+    (Conv2D<3, 4, 3>, ReLU),
+    (Conv2D<4, 8, 3>, ReLU),
+    (Conv2D<8, 16, 3>, ReLU),
+    Flatten2D,
+    Linear<7744, 10>,
+);
+
 fn main() {
-    panic!("`+nightly` required to run this example.")
-}
-
-#[cfg(feature = "nightly")]
-fn main() {
-    use dfdx::prelude::*;
-    use rand::thread_rng;
-
-    type Model = (
-        (Conv2D<3, 4, 3>, ReLU),
-        (Conv2D<4, 8, 3>, ReLU),
-        (Conv2D<8, 16, 3>, ReLU),
-        FlattenImage,
-        Linear<7744, 10>,
-    );
-
     let mut rng = thread_rng();
     let mut m: Model = Default::default();
     m.reset_params(&mut rng);
 
     // single image forward
     let x: Tensor3D<3, 28, 28> = TensorCreator::randn(&mut rng);
+
+    #[cfg(feature = "nightly")]
     let _: Tensor1D<10> = m.forward(x);
 
     // batched image forward
     let x: Tensor4D<32, 3, 28, 28> = TensorCreator::randn(&mut rng);
+
+    #[cfg(feature = "nightly")]
     let _: Tensor2D<32, 10> = m.forward(x);
 }
