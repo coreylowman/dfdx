718a1fe4c10192e18f12f9da6ce33b28b1ea3cf1
Dimas Leenman <skythedragon@outlook.com>
2022-10-24 19:39:26 +0200

    add into passes tests

--- src/nn/add_into.rs
+++ src/nn/add_into.rs
@@ -14,7 +14,7 @@ use crate::prelude::*;
 /// # use dfdx::prelude::*;
 /// type Model = AddInto<(Linear<2, 5>, Linear<3, 5>)>;
 /// let model: Model = Default::default();
-/// let _: Tensor1D<5> = model.forward((Tensor1D::<2>::zeros(), Teensor1D::<3>::zeros()));
+/// let _: Tensor1D<5> = model.forward((Tensor1D::<2>::zeros(), Tensor1D::<3>::zeros()));
 /// ```
 #[derive(Debug, Default, Clone)]
 pub struct AddInto<T>(pub T);
@@ -31,34 +31,6 @@ impl<T: ResetParams> ResetParams for AddInto<T> {
     }
 }
 
-impl<Input: Tensor, Mod: Module<Input>> Module<(Input, )> for AddInto<(Mod, )>
-{
-    type Output = Mod::Output;
-    
-    fn forward(&self, x: (Input, )) -> Self::Output {
-        let (head, ) = x;
-        
-        // no need for adding
-        self.0.0.forward(head)
-    
-    }
-}
-
-impl<Input: Tensor, Mod: ModuleMut<Input>> ModuleMut<(Input, )> for AddInto<(Mod, )> {
-    type Output = Mod::Output;
-    
-    fn forward_mut(&mut self, x: (Input, )) -> Self::Output {
-        let (head, ) = x;
-        self.0.0.forward_mut(head)
-    }
-}
-
-// so what we want to do:
-// split off the head from the tuple
-// put the tail into an AddInto
-// forward head and tail
-// add them together
-// return
 macro_rules! tuple_impls {
     ($head:ident $headin:ident [$($tails:ident $tailsin:ident),+]) => {
         impl<
@@ -66,36 +38,202 @@ macro_rules! tuple_impls {
             $headin: Tensor<Dtype = f32>,
             $($tailsin: Tensor<Dtype = f32>,)+
             $head: Module<$headin, Output = Output>,
-            $($tails: Module<$tailsin>,)+
+            $($tails: Module<$tailsin, Output = Output>,)+
         > Module<($headin, $($tailsin,)+)> for AddInto<($head, $($tails,)+)> {
             type Output = Output;
-            
+
             #[allow(non_snake_case)]
             fn forward(&self, x: ($headin, $($tailsin,)+)) -> Self::Output {
-                
+
                 // inputs
                 let ($head, $($tails),+) = x;
-                let ($head, tape) = $head.split_tape();
-                
+
                 // layers
                 let ($headin, $($tailsin),+) = &self.0;
-                
+
                 // forward
-                let ($head, $($tails),+) = ($headin.forward($head.put_tape(tape)), $($tailsin.forward($tails)),+);
-                
+                let ($head, $($tails),+) = ($headin.forward($head), $($tailsin.forward($tails)),+);
+
                 // add together
                 $(
-                    let $head = add($head, &$tails);
+                    let $head = add($head, $tails);
                 )+
-                
+
                 $head
             }
         }
-    }
-} 
+
+
+        impl<
+            Output: Tensor<Dtype = f32>,
+            $headin: Tensor<Dtype = f32>,
+            $($tailsin: Tensor<Dtype = f32>,)+
+            $head: Module<$headin, Output = Output>,
+            $($tails: Module<$tailsin, Output = Output>,)+
+        > ModuleMut<($headin, $($tailsin,)+)> for AddInto<($head, $($tails,)+)> {
+            type Output = Output;
+
+            #[allow(non_snake_case)]
+            fn forward_mut(&mut self, x: ($headin, $($tailsin,)+)) -> Self::Output {
+
+                // inputs
+                let ($head, $($tails),+) = x;
+
+                // layers
+                let ($headin, $($tailsin),+) = &self.0;
+
+                // forward
+                let ($head, $($tails),+) = ($headin.forward($head), $($tailsin.forward($tails)),+);
+
+                // add together
+                $(
+                    let $head = add($head, $tails);
+                )+
+
+                $head
+            }
+        }    }
+}
 
 tuple_impls!(A Ai [B Bi]);
 tuple_impls!(A Ai [B Bi, C Ci]);
 tuple_impls!(A Ai [B Bi, C Ci, D Di]);
 tuple_impls!(A Ai [B Bi, C Ci, D Di, E Ei]);
 tuple_impls!(A Ai [B Bi, C Ci, D Di, E Ei, F Fi]);
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{nn::tests::SimpleGradients, unique_id::HasUniqueId};
+
+    #[test]
+    fn test_add_into_2() {
+        type Model = AddInto<(Linear<2, 5>, Linear<3, 5>)>;
+        let m: Model = Default::default();
+        let _: Tensor1D<5, OwnedTape> =
+            m.forward((Tensor1D::zeros().traced(), Tensor1D::zeros().traced()));
+        let _: Tensor2D<3, 5, OwnedTape> = m.forward((
+            Tensor2D::<3, 2>::zeros().traced(),
+            Tensor2D::<3, 3>::zeros().traced(),
+        ));
+    }
+
+    #[test]
+    fn test_add_into_3() {
+        type Model = AddInto<(Linear<2, 5>, Linear<3, 5>, Linear<4, 5>)>;
+        let m: Model = Default::default();
+        let _: Tensor1D<5, OwnedTape> = m.forward((
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+        ));
+        let _: Tensor2D<3, 5, OwnedTape> = m.forward((
+            Tensor2D::<3, 2>::zeros().traced(),
+            Tensor2D::<3, 3>::zeros().traced(),
+            Tensor2D::<3, 4>::zeros().traced(),
+        ));
+    }
+
+    #[test]
+    fn test_add_into_4() {
+        type Model = AddInto<(Linear<2, 5>, Linear<3, 5>, Linear<4, 5>, Linear<5, 5>)>;
+        let m: Model = Default::default();
+        let _: Tensor1D<5, OwnedTape> = m.forward((
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+        ));
+        let _: Tensor2D<3, 5, OwnedTape> = m.forward((
+            Tensor2D::<3, 2>::zeros().traced(),
+            Tensor2D::<3, 3>::zeros().traced(),
+            Tensor2D::<3, 4>::zeros().traced(),
+            Tensor2D::<3, 5>::zeros().traced(),
+        ));
+    }
+
+    #[test]
+    fn test_add_into_5() {
+        type Model = AddInto<(
+            Linear<2, 5>,
+            Linear<3, 5>,
+            Linear<4, 5>,
+            Linear<5, 5>,
+            Linear<6, 5>,
+        )>;
+        let m: Model = Default::default();
+        let _: Tensor1D<5, OwnedTape> = m.forward((
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+        ));
+        let _: Tensor2D<3, 5, OwnedTape> = m.forward((
+            Tensor2D::<3, 2>::zeros().traced(),
+            Tensor2D::<3, 3>::zeros().traced(),
+            Tensor2D::<3, 4>::zeros().traced(),
+            Tensor2D::<3, 5>::zeros().traced(),
+            Tensor2D::<3, 6>::zeros().traced(),
+        ));
+    }
+
+    #[test]
+    fn test_add_into_6() {
+        
+type Model = AddInto<(
+            Linear<2, 5>,
+            Linear<3, 5>,
+            Linear<4, 5>,
+            Linear<5, 5>,
+            Linear<6, 5>,
+            Linear<7, 5>,
+        )>;
+        let m: Model = Default::default();
+        let _: Tensor1D<5, OwnedTape> = m.forward((
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+            Tensor1D::zeros().traced(),
+        ));
+        let _: Tensor2D<3, 5, OwnedTape> = m.forward((
+            Tensor2D::<3, 2>::zeros().traced(),
+            Tensor2D::<3, 3>::zeros().traced(),
+            Tensor2D::<3, 4>::zeros().traced(),
+            Tensor2D::<3, 5>::zeros().traced(),
+            Tensor2D::<3, 6>::zeros().traced(),
+            Tensor2D::<3, 7>::zeros().traced(),
+        ));
+     }
+
+    #[test]
+    fn test_missing_gradients() {
+        let mut model: AddInto<(Linear<5, 3>, Linear<5, 3>)> = Default::default();
+        let mut g: SimpleGradients = Default::default();
+
+        // no gradients present
+        let mut unused = Default::default();
+        model.update(&mut g, &mut unused);
+        assert_eq!(
+            &unused.ids,
+            &[
+                *model.0 .0.weight.id(),
+                *model.0 .0.bias.id(),
+                *model.0 .1.weight.id(),
+                *model.0 .1.bias.id()
+            ]
+        );
+
+        // weight gradient is present
+        g.0.mut_gradient(&model.0 .0.weight);
+        g.0.mut_gradient(&model.0 .0.bias);
+        g.0.mut_gradient(&model.0 .1.weight);
+        g.0.mut_gradient(&model.0 .1.bias);
+
+        let mut unused = Default::default();
+        model.update(&mut g, &mut unused);
+        assert!(unused.is_empty());
+    }
+}
