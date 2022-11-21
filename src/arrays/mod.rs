8cf8954357c8f828c8a40779b655d86925182c80
Corey Lowman <clowman1993@gmail.com>
2022-11-21 10:37:24 -0500

    Cleaning up arrays exports

--- src/arrays/mod.rs
+++ src/arrays/mod.rs
@@ -4,11 +4,13 @@ mod broadcasts;
 mod permutes;
 mod shape;
 
-pub(crate) use broadcasts::{BroadcastShapeTo, BroadcastStrides};
+pub(crate) use axes::AxesAsArray;
+pub(crate) use broadcasts::{ReduceShape, BroadcastStrides};
 pub(crate) use permutes::PermuteShapeTo;
 
-pub use broadcasts::ReduceShape;
-
-pub use as_rust_array::*;
-pub use axes::*;
-pub use shape::*;
+pub use as_rust_array::RustArrayRepr;
+pub use axes::{Axes2, Axes3, Axes4, Axes5, Axes6, Axis, HasAxes};
+pub use shape::{
+    Dim, Dtype, Dyn, HasDtype, HasShape, Rank0, Rank1, Rank2, Rank3, Rank4, Rank5, Rank6, Shape,
+    StridesFor, TryFromNumElements, C,
+};
