4858ea6c838cfe545d0d29fd8d205602e8b6e892
Corey Lowman <clowman1993@gmail.com>
2022-11-28 16:49:06 -0500

    Remove imports from device

--- src/tensor_ops/device.rs
+++ src/tensor_ops/device.rs
@@ -1,34 +1,4 @@
-use super::abs::AbsKernelOp;
-use super::add::{BinaryAddKernelOp, ScalarAddKernelOp};
-use super::bce::BCEKernelOp;
-use super::broadcast_to::BroadcastKernel;
-use super::clamp::ClampKernelOp;
-use super::cos::CosKernelOp;
-use super::div::{BinaryDivKernelOp, ScalarDivKernelOp};
-use super::dropout::DropoutKernelOp;
-use super::exp::ExpKernelOp;
-use super::huber_error::HuberErrorKernelOp;
-use super::ln::LnKernelOp;
-use super::matmul::*;
-use super::max_to::MaxReduceKernel;
-use super::maximum::MaximumKernelOp;
-use super::min_to::MinReduceKernel;
-use super::minimum::MinimumKernelOp;
-use super::mul::{BinaryMulKernelOp, ScalarMulKernelOp};
-use super::nans_to::NansToKernelOp;
-use super::negate::NegateKernelOp;
 use super::ops::{BinaryKernel, UnaryKernel};
-use super::pow::PowKernelOp;
-use super::relu::ReLUKernelOp;
-use super::select_to::{ReplaceAxisKernel, SelectAxisKernel, SelectBatchKernel};
-use super::sigmoid::SigmoidKernelOp;
-use super::sin::SinKernelOp;
-use super::sqrt::SqrtKernelOp;
-use super::square::SquareKernelOp;
-use super::sub::{BinarySubKernelOp, ScalarSubKernelOp};
-use super::sum_to::SumKernel;
-use super::tanh::TanhKernelOp;
-
 use crate::{
     arrays::Dtype,
     devices::{Cpu, DeviceStorage},
@@ -38,59 +8,59 @@ pub trait Device<E: Dtype>:
     DeviceStorage
 
     // broadcast & reduces
-    + BroadcastKernel<E>
-    + SumKernel<E>
-    + MaxReduceKernel<E>
-    + MinReduceKernel<E>
+    + super::broadcast_to::BroadcastKernel<E>
+    + super::sum_to::SumKernel<E>
+    + super::max_to::MaxReduceKernel<E>
+    + super::min_to::MinReduceKernel<E>
 
     // indexing
-    + SelectAxisKernel<E>
-    + ReplaceAxisKernel<E>
-    + SelectBatchKernel<E>
+    + super::select_to::SelectAxisKernel<E>
+    + super::select_to::ReplaceAxisKernel<E>
+    + super::select_to::SelectBatchKernel<E>
 
     // matmuls
-    + VecMatKernel<E>
-    + MatMatKernel<E>
-    + VecVecKernel<E>
-    + MatMatBrKernel<E>
-    + MatMatBatch3Kernel<E>
-    + MatMatBatch4Kernel<E>
+    + super::matmul::VecMatKernel<E>
+    + super::matmul::MatMatKernel<E>
+    + super::matmul::VecVecKernel<E>
+    + super::matmul::MatMatBrKernel<E>
+    + super::matmul::MatMatBatch3Kernel<E>
+    + super::matmul::MatMatBatch4Kernel<E>
 
     // scalar arithmetic
-    + UnaryKernel<ScalarAddKernelOp<E>, E>
-    + UnaryKernel<ScalarDivKernelOp<E>, E>
-    + UnaryKernel<ScalarMulKernelOp<E>, E>
-    + UnaryKernel<ScalarSubKernelOp<E>, E>
+    + UnaryKernel<super::add::ScalarAddKernelOp<E>, E>
+    + UnaryKernel<super::sub::ScalarSubKernelOp<E>, E>
+    + UnaryKernel<super::mul::ScalarMulKernelOp<E>, E>
+    + UnaryKernel<super::div::ScalarDivKernelOp<E>, E>
 
     // binary arithmetic
-    + BinaryKernel<BinaryAddKernelOp, E>
-    + BinaryKernel<BinaryDivKernelOp, E>
-    + BinaryKernel<BinaryMulKernelOp, E>
-    + BinaryKernel<BinarySubKernelOp, E>
+    + BinaryKernel<super::add::BinaryAddKernelOp, E>
+    + BinaryKernel<super::sub::BinarySubKernelOp, E>
+    + BinaryKernel<super::mul::BinaryMulKernelOp, E>
+    + BinaryKernel<super::div::BinaryDivKernelOp, E>
 
     // unary
-    + UnaryKernel<AbsKernelOp, E>
-    + UnaryKernel<ClampKernelOp<E>, E>
-    + UnaryKernel<CosKernelOp, E>
-    + UnaryKernel<DropoutKernelOp, E>
-    + UnaryKernel<ExpKernelOp, E>
-    + UnaryKernel<LnKernelOp, E>
-    + UnaryKernel<NansToKernelOp<E>, E>
-    + UnaryKernel<NegateKernelOp, E>
-    + UnaryKernel<ReLUKernelOp, E>
-    + UnaryKernel<SigmoidKernelOp, E>
-    + UnaryKernel<SinKernelOp, E>
-    + UnaryKernel<SqrtKernelOp, E>
-    + UnaryKernel<SquareKernelOp, E>
-    + UnaryKernel<TanhKernelOp, E>
-    + UnaryKernel<PowKernelOp<E>, E>
-    + UnaryKernel<PowKernelOp<i32>, E>
+    + UnaryKernel<super::abs::AbsKernelOp, E>
+    + UnaryKernel<super::clamp::ClampKernelOp<E>, E>
+    + UnaryKernel<super::cos::CosKernelOp, E>
+    + UnaryKernel<super::dropout::DropoutKernelOp, E>
+    + UnaryKernel<super::exp::ExpKernelOp, E>
+    + UnaryKernel<super::ln::LnKernelOp, E>
+    + UnaryKernel<super::nans_to::NansToKernelOp<E>, E>
+    + UnaryKernel<super::negate::NegateKernelOp, E>
+    + UnaryKernel<super::relu::ReLUKernelOp, E>
+    + UnaryKernel<super::sigmoid::SigmoidKernelOp, E>
+    + UnaryKernel<super::sin::SinKernelOp, E>
+    + UnaryKernel<super::sqrt::SqrtKernelOp, E>
+    + UnaryKernel<super::square::SquareKernelOp, E>
+    + UnaryKernel<super::tanh::TanhKernelOp, E>
+    + UnaryKernel<super::pow::PowKernelOp<E>, E>
+    + UnaryKernel<super::pow::PowKernelOp<i32>, E>
 
     // binary
-    + BinaryKernel<BCEKernelOp, E>
-    + BinaryKernel<HuberErrorKernelOp<E>, E>
-    + BinaryKernel<MaximumKernelOp, E>
-    + BinaryKernel<MinimumKernelOp, E>
+    + BinaryKernel<super::bce::BCEKernelOp, E>
+    + BinaryKernel<super::huber_error::HuberErrorKernelOp<E>, E>
+    + BinaryKernel<super::maximum::MaximumKernelOp, E>
+    + BinaryKernel<super::minimum::MinimumKernelOp, E>
 {
 }
 
