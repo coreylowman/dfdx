1a94f07361fd7bbde30697972fd22ba0d48c8d4d
Corey Lowman <clowman1993@gmail.com>
2022-12-06 16:54:06 -0500

    Fixing example 11

--- examples/11-multi-headed.rs
+++ examples/11-multi-headed.rs
@@ -1,16 +1,20 @@
 //! Demonstrates how to build a neural network that has multiple
 //! outputs using `SplitInto`.
 
-use dfdx::nn::{Linear, Module, SplitInto};
-use dfdx::tensor::{tensor, Tensor1D};
+use dfdx::{
+    nn::{BuildModule, Linear, Module, SplitInto},
+    shapes::Rank1,
+    tensor::{Cpu, Tensor, TensorFromArray},
+};
 
 fn main() {
+    let dev: Cpu = Default::default();
+
     // SplitInto accepts a tuple of modules. Each one of the items in the
     // tuple must accept the same type of input.
     // Note that here, both of the linears have the same size input (1)
-    let m: SplitInto<(Linear<1, 3>, Linear<1, 5>)> = Default::default();
+    let m: SplitInto<(Linear<1, 3>, Linear<1, 5>)> = BuildModule::build(&dev);
 
     // when we forward data through, we get a tuple back!
-    let x = tensor([1.0]);
-    let _: (Tensor1D<3>, Tensor1D<5>) = m.forward(x);
+    let _: (Tensor<Rank1<3>, f32, Cpu>, Tensor<Rank1<5>, f32, Cpu>) = m.forward(dev.tensor([1.0]));
 }
