d9267f6199174da39f886313faa8fd0c66170098
Corey Lowman <clowman1993@gmail.com>
2022-12-06 16:52:21 -0500

    Fixing 07 example and adding resnet18 example

--- examples/07-custom-module.rs
+++ examples/07-custom-module.rs
@@ -1,48 +1,65 @@
 //! Demonstrates how to build a custom [nn::Module] without using tuples
 
-use rand::prelude::*;
-
-use dfdx::gradients::{CanUpdateWithGradients, OwnedTape, ParamUpdater, Tape, UnusedTensors};
-use dfdx::nn::{BuildModule, Linear, Module, ReLU};
-use dfdx::tensor::{Tensor1D, Tensor2D, TensorCreator};
+use dfdx::{
+    gradients::Tape,
+    nn::{self, Module},
+    optim::{CanUpdateWithGradients, UnusedTensors, UpdateParams},
+    shapes::{Rank1, Rank2},
+    tensor::{Cpu, HasErr, RandnTensor, Tensor},
+};
 
 /// Custom model struct
 /// This case is trivial and should be done with a tuple of linears and relus,
 /// but it demonstrates how to build models with custom behavior
-#[derive(Default)]
 struct Mlp<const IN: usize, const INNER: usize, const OUT: usize> {
-    l1: Linear<IN, INNER>,
-    l2: Linear<INNER, OUT>,
-    relu: ReLU,
+    l1: nn::Linear<IN, INNER>,
+    l2: nn::Linear<INNER, OUT>,
+    relu: nn::ReLU,
 }
 
-// ResetParams lets you randomize a model's parameters
-impl<const IN: usize, const INNER: usize, const OUT: usize> BuildModule for Mlp<IN, INNER, OUT> {
-    fn reset_params<R: rand::Rng>(&mut self, rng: &mut R) {
-        self.l1.reset_params(rng);
-        self.l2.reset_params(rng);
-        self.relu.reset_params(rng);
+// BuildModule lets you randomize a model's parameters
+impl<const IN: usize, const INNER: usize, const OUT: usize> nn::BuildModule<Cpu, f32>
+    for Mlp<IN, INNER, OUT>
+{
+    fn try_build(device: &Cpu) -> Result<Self, <Cpu as HasErr>::Err> {
+        Ok(Self {
+            l1: nn::BuildModule::try_build(device)?,
+            l2: nn::BuildModule::try_build(device)?,
+            relu: nn::ReLU,
+        })
+    }
+    fn try_reset_params(&mut self) -> Result<(), <Cpu as HasErr>::Err> {
+        self.l1.try_reset_params()?;
+        self.l2.try_reset_params()?;
+        Ok(())
     }
 }
 
 // CanUpdateWithGradients lets you update a model's parameters using gradients
-impl<const IN: usize, const INNER: usize, const OUT: usize> CanUpdateWithGradients
+impl<const IN: usize, const INNER: usize, const OUT: usize> CanUpdateWithGradients<Cpu, f32>
     for Mlp<IN, INNER, OUT>
 {
-    fn update<G: ParamUpdater>(&mut self, grads: &mut G, unused: &mut UnusedTensors) {
-        self.l1.update(grads, unused);
-        self.l2.update(grads, unused);
-        self.relu.update(grads, unused);
+    fn update<U>(
+        &mut self,
+        updater: &mut U,
+        unused: &mut UnusedTensors,
+    ) -> Result<(), <Cpu as HasErr>::Err>
+    where
+        U: UpdateParams<Cpu, f32>,
+    {
+        self.l1.update(updater, unused)?;
+        self.l2.update(updater, unused)?;
+        Ok(())
     }
 }
 
 // impl Module for single item
-impl<const IN: usize, const INNER: usize, const OUT: usize> Module<Tensor1D<IN>>
+impl<const IN: usize, const INNER: usize, const OUT: usize> nn::Module<Tensor<Rank1<IN>, f32, Cpu>>
     for Mlp<IN, INNER, OUT>
 {
-    type Output = Tensor1D<OUT>;
+    type Output = Tensor<Rank1<OUT>, f32, Cpu>;
 
-    fn forward(&self, x: Tensor1D<IN>) -> Self::Output {
+    fn forward(&self, x: Tensor<Rank1<IN>, f32, Cpu>) -> Self::Output {
         let x = self.l1.forward(x);
         let x = self.relu.forward(x);
         self.l2.forward(x)
@@ -50,33 +67,28 @@ impl<const IN: usize, const INNER: usize, const OUT: usize> Module<Tensor1D<IN>>
 }
 
 // impl Module for batch of items
-impl<const BATCH: usize, const IN: usize, const INNER: usize, const OUT: usize, TAPE: Tape>
-    Module<Tensor2D<BATCH, IN, TAPE>> for Mlp<IN, INNER, OUT>
+impl<const BATCH: usize, const IN: usize, const INNER: usize, const OUT: usize, T: Tape<Cpu>>
+    nn::Module<Tensor<Rank2<BATCH, IN>, f32, Cpu, T>> for Mlp<IN, INNER, OUT>
 {
-    type Output = Tensor2D<BATCH, OUT, TAPE>;
+    type Output = Tensor<Rank2<BATCH, OUT>, f32, Cpu, T>;
 
-    fn forward(&self, x: Tensor2D<BATCH, IN, TAPE>) -> Self::Output {
+    fn forward(&self, x: Tensor<Rank2<BATCH, IN>, f32, Cpu, T>) -> Self::Output {
         let x = self.l1.forward(x);
         let x = self.relu.forward(x);
         self.l2.forward(x)
     }
 }
 
-const BATCH_SIZE: usize = 32;
-
 fn main() {
     // Rng for generating model's params
-    let mut rng = StdRng::seed_from_u64(0);
+    let dev: Cpu = Default::default();
 
     // Construct model
-    let mut model: Mlp<10, 512, 10> = Mlp::default();
-    model.reset_params(&mut rng);
+    let model: Mlp<10, 512, 20> = nn::BuildModule::build(&dev);
 
     // Forward pass with a single sample
-    let sample: Tensor1D<10> = Tensor1D::randn(&mut rng);
-    let _: Tensor1D<10> = model.forward(sample);
+    let _: Tensor<Rank1<20>, f32, Cpu> = model.forward(dev.randn::<Rank1<10>>());
 
     // Forward pass with a batch of samples
-    let batch: Tensor2D<BATCH_SIZE, 10> = Tensor2D::randn(&mut rng);
-    let _: Tensor2D<BATCH_SIZE, 10, OwnedTape> = model.forward(batch.trace());
+    let _: Tensor<Rank2<32, 20>, f32, Cpu, _> = model.forward(dev.randn::<Rank2<32, 10>>().trace());
 }
