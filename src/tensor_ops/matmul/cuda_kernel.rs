31996d3fb36a38a65d3b18e15e8d6629a01a717e
Corey Lowman <clowman1993@gmail.com>
2023-01-07 17:08:00 +0000

    matvec normal, transpose not working

--- src/tensor_ops/matmul/cuda_kernel.rs
+++ src/tensor_ops/matmul/cuda_kernel.rs
@@ -15,29 +15,32 @@ impl super::VecVecKernel<f32> for Cuda {
         lhs: &Self::Storage<(M,), f32>,
         rhs: &Self::Storage<(N,), f32>,
     ) -> Result<Self::Storage<(M, N), f32>, Self::Err> {
-        let shape = (lhs.shape.0, rhs.shape.0);
+        let (m,) = lhs.shape;
+        let (n,) = rhs.shape;
+        let k = Const::<1>;
+        let shape = (m, n);
         let mut storage = self.dev.alloc_zeros_async::<f32>(shape.num_elements())?;
 
         // TODO: use strides
-        let m = lhs.shape.0.size() as i32;
-        let n = rhs.shape.0.size() as i32;
-        let k = 1;
         unsafe {
             // storage = lhs * rhs
+            let m_op = m.size() as i32;
+            let n_op = n.size() as i32;
+            let k_op = k.size() as i32;
             self.blas.gemm_async(
                 cublasOperation_t::CUBLAS_OP_N,
                 cublasOperation_t::CUBLAS_OP_N,
-                n,
-                m,
-                k,
+                n_op,
+                m_op,
+                k_op,
                 1.0,
                 rhs.data.as_ref(),
-                n,
+                n_op,
                 lhs.data.as_ref(),
-                k,
+                k_op,
                 0.0,
                 &mut storage,
-                n,
+                n_op,
             )
         }?;
 
@@ -56,50 +59,52 @@ impl super::VecVecKernel<f32> for Cuda {
         grad_rhs: &mut Self::Storage<(N,), f32>,
         grad_out: &Self::Storage<(M, N), f32>,
     ) -> Result<(), Self::Err> {
+        let (m, n) = grad_out.shape;
+        let k = Const::<1>;
         // TODO use strides
         {
             // grad_lhs += grad_out * rhs
-            let m = lhs.shape.0.size() as i32;
-            let n = 1;
-            let k = rhs.shape.0.size() as i32;
+            let m_op = m.size() as i32;
+            let n_op = k.size() as i32;
+            let k_op = n.size() as i32;
             unsafe {
                 self.blas.gemm_async(
                     cublasOperation_t::CUBLAS_OP_N,
                     cublasOperation_t::CUBLAS_OP_N,
-                    n,
-                    m,
-                    k,
+                    n_op,
+                    m_op,
+                    k_op,
                     1.0,
                     rhs.data.as_ref(),
-                    n,
+                    n_op,
                     grad_out.data.as_ref(),
-                    k,
+                    k_op,
                     1.0,
                     Arc::make_mut(&mut grad_lhs.data),
-                    n,
+                    n_op,
                 )
             }?;
         }
         {
             // grad_rhs += lhs * grad_out
-            let m = 1;
-            let n = rhs.shape.0.size() as i32;
-            let k = lhs.shape.0.size() as i32;
+            let m_op = k.size() as i32;
+            let n_op = n.size() as i32;
+            let k_op = m.size() as i32;
             unsafe {
                 self.blas.gemm_async(
                     cublasOperation_t::CUBLAS_OP_N,
                     cublasOperation_t::CUBLAS_OP_N,
-                    n,
-                    m,
-                    k,
+                    n_op,
+                    m_op,
+                    k_op,
                     1.0,
                     grad_out.data.as_ref(),
-                    n,
+                    n_op,
                     lhs.data.as_ref(),
-                    k,
+                    k_op,
                     1.0,
                     Arc::make_mut(&mut grad_rhs.data),
-                    n,
+                    n_op,
                 )
             }?;
         }
@@ -113,7 +118,39 @@ impl super::VecMatKernel<f32> for Cuda {
         lhs: &Self::Storage<(Const<K>,), f32>,
         rhs: &Self::Storage<(Const<K>, N), f32>,
     ) -> Result<Self::Storage<(N,), f32>, Self::Err> {
-        todo!()
+        let m = Const::<1>;
+        let (k, n) = rhs.shape;
+        let shape = (n,);
+        let mut storage = self.dev.alloc_zeros_async::<f32>(shape.num_elements())?;
+
+        // TODO: use strides
+        unsafe {
+            // storage = lhs * rhs
+            let m_op = m.size() as i32;
+            let n_op = n.size() as i32;
+            let k_op = k.size() as i32;
+            self.blas.gemm_async(
+                cublasOperation_t::CUBLAS_OP_N,
+                cublasOperation_t::CUBLAS_OP_N,
+                n_op,
+                m_op,
+                k_op,
+                1.0,
+                rhs.data.as_ref(),
+                n_op,
+                lhs.data.as_ref(),
+                k_op,
+                0.0,
+                &mut storage,
+                n_op,
+            )
+        }?;
+
+        Ok(CudaArray {
+            data: Arc::new(storage),
+            shape,
+            strides: shape.strides(),
+        })
     }
     fn backward<const K: usize, N: Dim>(
         &self,
@@ -123,7 +160,56 @@ impl super::VecMatKernel<f32> for Cuda {
         grad_rhs: &mut Self::Storage<(Const<K>, N), f32>,
         grad_out: &Self::Storage<(N,), f32>,
     ) -> Result<(), Self::Err> {
-        todo!()
+        let m = Const::<1>;
+        let (k, n) = rhs.shape;
+        // TODO use strides
+        {
+            // grad_lhs += grad_out * rhs^T
+            let m_op = m.size() as i32;
+            let n_op = k.size() as i32;
+            let k_op = n.size() as i32;
+            unsafe {
+                self.blas.gemm_async(
+                    cublasOperation_t::CUBLAS_OP_T,
+                    cublasOperation_t::CUBLAS_OP_N,
+                    n_op,
+                    m_op,
+                    k_op,
+                    1.0,
+                    rhs.data.as_ref(),
+                    k_op,
+                    grad_out.data.as_ref(),
+                    k_op,
+                    1.0,
+                    Arc::make_mut(&mut grad_lhs.data),
+                    n_op,
+                )
+            }?;
+        }
+        {
+            // grad_rhs += lhs * grad_out
+            let m_op = k.size() as i32;
+            let n_op = n.size() as i32;
+            let k_op = m.size() as i32;
+            unsafe {
+                self.blas.gemm_async(
+                    cublasOperation_t::CUBLAS_OP_N,
+                    cublasOperation_t::CUBLAS_OP_N,
+                    n_op,
+                    m_op,
+                    k_op,
+                    1.0,
+                    grad_out.data.as_ref(),
+                    n_op,
+                    lhs.data.as_ref(),
+                    k_op,
+                    1.0,
+                    Arc::make_mut(&mut grad_rhs.data),
+                    n_op,
+                )
+            }?;
+        }
+        Ok(())
     }
 }
 
