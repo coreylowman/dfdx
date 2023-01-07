5b70bf097a7143d33594e7902b1ccaaf25d340e9
Corey Lowman <clowman1993@gmail.com>
2023-01-07 16:54:28 +0000

    vecvec cuda kernel

--- src/tensor_ops/matmul/cuda_kernel.rs
+++ src/tensor_ops/matmul/cuda_kernel.rs
@@ -1,4 +1,13 @@
-use crate::{shapes::*, tensor::Cuda};
+use crate::{
+    shapes::*,
+    tensor::cuda::{Cuda, CudaArray},
+};
+
+use cudarc::{
+    blas::{Gemm, Gemv},
+    cublas::sys::cublasOperation_t,
+};
+use std::sync::Arc;
 
 impl super::VecVecKernel<f32> for Cuda {
     fn forward<M: Dim, N: Dim>(
@@ -6,8 +15,39 @@ impl super::VecVecKernel<f32> for Cuda {
         lhs: &Self::Storage<(M,), f32>,
         rhs: &Self::Storage<(N,), f32>,
     ) -> Result<Self::Storage<(M, N), f32>, Self::Err> {
-        todo!()
+        let shape = (lhs.shape.0, rhs.shape.0);
+        let mut storage = self.dev.alloc_zeros_async::<f32>(shape.num_elements())?;
+
+        // TODO: use strides
+        let m = lhs.shape.0.size() as i32;
+        let n = rhs.shape.0.size() as i32;
+        let k = 1;
+        unsafe {
+            // storage = lhs * rhs
+            self.blas.gemm_async(
+                cublasOperation_t::CUBLAS_OP_N,
+                cublasOperation_t::CUBLAS_OP_N,
+                n,
+                m,
+                k,
+                1.0,
+                rhs.data.as_ref(),
+                n,
+                lhs.data.as_ref(),
+                k,
+                0.0,
+                &mut storage,
+                n,
+            )
+        }?;
+
+        Ok(CudaArray {
+            data: Arc::new(storage),
+            shape,
+            strides: shape.strides(),
+        })
     }
+
     fn backward<M: Dim, N: Dim>(
         &self,
         lhs: &Self::Storage<(M,), f32>,
@@ -16,7 +56,54 @@ impl super::VecVecKernel<f32> for Cuda {
         grad_rhs: &mut Self::Storage<(N,), f32>,
         grad_out: &Self::Storage<(M, N), f32>,
     ) -> Result<(), Self::Err> {
-        todo!()
+        // TODO use strides
+        {
+            // grad_lhs += grad_out * rhs
+            let m = lhs.shape.0.size() as i32;
+            let n = 1;
+            let k = rhs.shape.0.size() as i32;
+            unsafe {
+                self.blas.gemm_async(
+                    cublasOperation_t::CUBLAS_OP_N,
+                    cublasOperation_t::CUBLAS_OP_N,
+                    n,
+                    m,
+                    k,
+                    1.0,
+                    rhs.data.as_ref(),
+                    n,
+                    grad_out.data.as_ref(),
+                    k,
+                    1.0,
+                    Arc::make_mut(&mut grad_lhs.data),
+                    n,
+                )
+            }?;
+        }
+        {
+            // grad_rhs += lhs * grad_out
+            let m = 1;
+            let n = rhs.shape.0.size() as i32;
+            let k = lhs.shape.0.size() as i32;
+            unsafe {
+                self.blas.gemm_async(
+                    cublasOperation_t::CUBLAS_OP_N,
+                    cublasOperation_t::CUBLAS_OP_N,
+                    n,
+                    m,
+                    k,
+                    1.0,
+                    grad_out.data.as_ref(),
+                    n,
+                    lhs.data.as_ref(),
+                    k,
+                    1.0,
+                    Arc::make_mut(&mut grad_rhs.data),
+                    n,
+                )
+            }?;
+        }
+        Ok(())
     }
 }
 
