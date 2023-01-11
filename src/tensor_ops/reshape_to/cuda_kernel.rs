82839eca5bd53b007a313c74f668b8886b1ce6fd
Nathan Koppel <nathankoppel0@gmail.com>
2023-01-11 12:10:50 -0600

    implement striding logic

--- src/tensor_ops/reshape_to/cuda_kernel.rs
+++ src/tensor_ops/reshape_to/cuda_kernel.rs
@@ -12,12 +12,12 @@ const FWD_FN_NAME: &str = "reshape_forward";
 const BWD_FN_NAME: &str = "reshape_backward";
 const ALL_FN_NAMES: [&str; 2] = [FWD_FN_NAME, BWD_FN_NAME];
 
-impl<E: Dtype + ValidAsZeroBits> super::ReshapeKernel<E> for Cuda {
+impl super::ReshapeKernel<f32> for Cuda {
     fn forward<Src: Shape, Dst: Shape>(
         &self,
         dst: Dst,
-        inp: &Self::Storage<Src, E>,
-    ) -> Result<Self::Storage<Dst, E>, Self::Err>
+        inp: &Self::Storage<Src, f32>,
+    ) -> Result<Self::Storage<Dst, f32>, Self::Err>
     where
         Src: HasSameNumelAs<Dst>,
     {
@@ -27,14 +27,25 @@ impl<E: Dtype + ValidAsZeroBits> super::ReshapeKernel<E> for Cuda {
         }
 
         let numel = inp.data.len();
-        let mut storage = self.dev.alloc_zeros_async::<E>(numel)?;
+        let mut storage = self.dev.alloc_zeros_async::<f32>(numel)?;
+
+        let inp_dims: CudaSlice<usize> = self.dev.take_async(inp.shape.concrete().into())?;
+        let dst_dims: CudaSlice<usize> = self.dev.take_async(dst.concrete().into())?;
+        let inp_strides: CudaSlice<usize> = self.dev.take_async(inp.strides.into())?;
+        let dst_strides: CudaSlice<usize> = self.dev.take_async(dst.strides().into())?;
 
         let fwd_fn = self.dev.get_func(MODULE_NAME, FWD_FN_NAME).unwrap();
         let cfg = LaunchConfig::for_num_elems(numel as u32);
         let params = (
             numel,             // const size_t numel,
             inp.data.as_ref(), // const float *inp,
+            Src::NUM_DIMS,     // const size_t inp_num_dims,
+            &inp_dims,         // const size_t *inp_dims,
+            &inp_strides,      // const size_t *inp_strides,
             &mut storage,      // float *out
+            Dst::NUM_DIMS,     // const size_t out_num_dims,
+            &dst_dims,         // const size_t *out_dims,
+            &dst_strides       // const size_t *out_strides,
         );
         unsafe { fwd_fn.launch_async(cfg, params) }?;
 
@@ -47,19 +58,31 @@ impl<E: Dtype + ValidAsZeroBits> super::ReshapeKernel<E> for Cuda {
 
     fn backward<Src: Shape, Dst: Shape>(
         &self,
-        grad_inp: &mut Self::Storage<Src, E>,
-        grad_out: &Self::Storage<Dst, E>,
+        grad_inp: &mut Self::Storage<Src, f32>,
+        grad_out: &Self::Storage<Dst, f32>,
     ) -> Result<(), Self::Err>
     where
         Src: HasSameNumelAs<Dst>,
     {
         let bwd_fn = self.dev.get_func(MODULE_NAME, BWD_FN_NAME).unwrap();
         let numel = grad_inp.data.len();
+
+        let inp_dims: CudaSlice<usize> = self.dev.take_async(grad_inp.shape.concrete().into())?;
+        let out_dims: CudaSlice<usize> = self.dev.take_async(grad_out.shape.concrete().into())?;
+        let inp_strides: CudaSlice<usize> = self.dev.take_async(grad_inp.strides.into())?;
+        let out_strides: CudaSlice<usize> = self.dev.take_async(grad_out.strides.into())?;
+
         let cfg = LaunchConfig::for_num_elems(numel as u32);
         let params = (
             numel,                             // const size_t numel,
             Arc::make_mut(&mut grad_inp.data), // float *grad_inp,
-            grad_out.data.as_ref(),            // const float *grad_out
+            Src::NUM_DIMS,                     // const size_t inp_num_dims,
+            &inp_dims,                         // const size_t *inp_dims,
+            &inp_strides,                      // const size_t *inp_strides,
+            grad_out.data.as_ref(),            // const float *grad_out,
+            Dst::NUM_DIMS,                     // const size_t out_num_dims,
+            &out_dims,                         // const size_t *out_dims,
+            &out_strides,                      // const size_t *out_strides
         );
         unsafe { bwd_fn.launch_async(cfg, params) }?;
         Ok(())
