c0a0e515476d9aa6c59a09cdf9b8de798222a233
Corey Lowman <clowman1993@gmail.com>
2023-01-17 14:09:51 +0000

    #340 Adding dropout cuda kernel

---
+++ src/tensor_ops/dropout/dropout.cu
@@ -0,0 +1,30 @@
+extern "C" __global__ void dropout_forward(
+    const float prob,
+    const size_t numel,
+    const float *inp,
+    const float *noise,
+    float *out
+) {
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i >= numel) {
+        return;
+    }
+
+    float scalar = (noise[i] < prob) ? 0.0 : (1.0 / (1.0 - prob));
+    out[i] = inp[i] * scalar;
+}
+
+extern "C" __global__ void dropout_backward(
+    const float prob,
+    const size_t numel,
+    const float *noise,
+    float *grad_inp,
+    const float *grad_out
+) {
+    unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i >= numel) {
+        return;
+    }
+
+    grad_inp[i] += (noise[i] < prob) ? 0.0 : (grad_out[i] / (1.0 - prob));
+}
