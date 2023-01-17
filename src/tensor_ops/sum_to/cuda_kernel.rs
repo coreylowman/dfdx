6ffa87d82896858357d64b224d717f37f92054af
nkoppel <nathankoppel0@gmail.com>
2023-01-17 19:40:49 +0000

    Modify sum_to cuda kernel to not need atomic adds in backwards (#367)
    * modify sum_to cuda kernel to not need atomic adds
    
    * improve readability of sum_to cuda kernel

--- src/tensor_ops/sum_to/cuda_kernel.rs
+++ src/tensor_ops/sum_to/cuda_kernel.rs
@@ -37,11 +37,15 @@ impl super::SumKernel<f32> for Cuda {
 
         let mut storage = self.dev.alloc_zeros_async::<f32>(dst.num_elements())?;
 
-        let inp_numel = inp.shape.num_elements();
-        let cfg = LaunchConfig::for_num_elems(inp_numel as u32);
+        let physical_numel = inp.data.len();
+        let virtual_numel = inp.shape.num_elements();
+        let elems_per_thread = (virtual_numel / physical_numel) as f32;
+
+        let cfg = LaunchConfig::for_num_elems(physical_numel as u32);
         let params = (
-            inp_numel,         // size_t numel,
-            Src::NUM_DIMS,     // size_t num_dims,
+            physical_numel,    // const size_t numel,
+            Src::NUM_DIMS,     // const size_t num_dims,
+            elems_per_thread,  // const float elems_per_thread,
             &dims,             // const size_t *dims,
             inp.data.as_ref(), // const float *inp,
             &inp_strides,      // const size_t *inp_strides,
@@ -72,11 +76,15 @@ impl super::SumKernel<f32> for Cuda {
             BroadcastStridesTo::<Src, Ax>::broadcast_strides(&grad_out.shape, grad_out.strides);
         let out_strides: CudaSlice<usize> = self.dev.take_async(out_strides.into())?;
 
-        let inp_numel = grad_inp.shape.num_elements();
-        let cfg = LaunchConfig::for_num_elems(inp_numel as u32);
+        let physical_numel = grad_inp.data.len();
+        let virtual_numel = grad_inp.shape.num_elements();
+        let elems_per_thread = (virtual_numel / physical_numel) as f32;
+
+        let cfg = LaunchConfig::for_num_elems(physical_numel as u32);
         let params = (
-            inp_numel,                         // size_t numel,
-            Src::NUM_DIMS,                     // size_t num_dims,
+            physical_numel,                    // const size_t numel,
+            Src::NUM_DIMS,                     // const size_t num_dims,
+            elems_per_thread,                  // const float elems_per_thread,
             &dims,                             // const size_t *dims,
             Arc::make_mut(&mut grad_inp.data), // float *grad_inp,
             &inp_strides,                      // const size_t *inp_strides,
