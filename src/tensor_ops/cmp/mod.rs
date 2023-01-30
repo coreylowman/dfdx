7b2a81b4bf18c2ee333d65f2d841f1f7218e5124
Viliam Vadocz <viliam.vadocz@gmail.com>
2023-01-30 03:32:35 +0100

    Add documentation for comparison operations

--- src/tensor_ops/cmp/mod.rs
+++ src/tensor_ops/cmp/mod.rs
@@ -49,7 +49,24 @@ pub enum GeKernelOp {}
 pub enum LtKernelOp {}
 pub enum LeKernelOp {}
 
+/// Element-wise equality comparison. `==`
 ///
+/// Examples:
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([1.2, 3.4, -0.5, -6.7]);
+/// let b = dev.tensor([1.2, 0.0, 3.14, -6.7]);
+/// let r = a.eq(&b);
+/// assert_eq!(r.array(), [true, false, false, true]);
+/// ```
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([-2, -1, 0, 1, 2]);
+/// let r = a.scalar_eq(1);
+/// assert_eq!(r.array(), [false, false, false, true, false]);
+/// ```
 pub fn eq<S: Shape, E: Unit, D: CmpKernel<EqKernelOp, E>, T: Tape<D>>(
     lhs: &Tensor<S, E, D, T>,
     rhs: &Tensor<S, E, D, T>,
@@ -57,7 +74,24 @@ pub fn eq<S: Shape, E: Unit, D: CmpKernel<EqKernelOp, E>, T: Tape<D>>(
     lhs.eq(rhs)
 }
 
+/// Element-wise inequality comparison. `!=`
 ///
+/// Examples:
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([1.2, 3.4, -0.5, -6.7]);
+/// let b = dev.tensor([1.2, 0.0, 3.14, -6.7]);
+/// let r = a.ne(&b);
+/// assert_eq!(r.array(), [false, true, true, false]);
+/// ```
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([-2, -1, 0, 1, 2]);
+/// let r = a.scalar_ne(1);
+/// assert_eq!(r.array(), [true, true, true, false, true]);
+/// ```
 pub fn ne<S: Shape, E: Unit, D: CmpKernel<NeKernelOp, E>, T: Tape<D>>(
     lhs: &Tensor<S, E, D, T>,
     rhs: &Tensor<S, E, D, T>,
@@ -65,7 +99,24 @@ pub fn ne<S: Shape, E: Unit, D: CmpKernel<NeKernelOp, E>, T: Tape<D>>(
     lhs.ne(rhs)
 }
 
+/// Element-wise strictly greater than comparison. `>`
 ///
+/// Examples:
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([1.2, 3.4, -0.5, -1.0]);
+/// let b = dev.tensor([1.2, 0.0, 3.14, -6.7]);
+/// let r = a.gt(&b);
+/// assert_eq!(r.array(), [false, true, false, true]);
+/// ```
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([-2, -1, 0, 1, 2]);
+/// let r = a.scalar_gt(-1);
+/// assert_eq!(r.array(), [false, false, true, true, true]);
+/// ```
 pub fn gt<S: Shape, E: Unit, D: CmpKernel<GtKernelOp, E>, T: Tape<D>>(
     lhs: &Tensor<S, E, D, T>,
     rhs: &Tensor<S, E, D, T>,
@@ -73,7 +124,24 @@ pub fn gt<S: Shape, E: Unit, D: CmpKernel<GtKernelOp, E>, T: Tape<D>>(
     lhs.gt(rhs)
 }
 
+/// Element-wise greater than or equals comparison. `>=`
 ///
+/// Examples:
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([1.2, 3.4, -0.5, -1.0]);
+/// let b = dev.tensor([1.2, 0.0, 3.14, -6.7]);
+/// let r = a.ge(&b);
+/// assert_eq!(r.array(), [true, true, false, true]);
+/// ```
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([-2, -1, 0, 1, 2]);
+/// let r = a.scalar_ge(-1);
+/// assert_eq!(r.array(), [false, true, true, true, true]);
+/// ```
 pub fn ge<S: Shape, E: Unit, D: CmpKernel<GeKernelOp, E>, T: Tape<D>>(
     lhs: &Tensor<S, E, D, T>,
     rhs: &Tensor<S, E, D, T>,
@@ -81,7 +149,24 @@ pub fn ge<S: Shape, E: Unit, D: CmpKernel<GeKernelOp, E>, T: Tape<D>>(
     lhs.ge(rhs)
 }
 
+/// Element-wise strictly less than comparison. `<`
 ///
+/// Examples:
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([1.2, 3.4, -0.5, -1.0]);
+/// let b = dev.tensor([1.2, 0.0, 3.14, -6.7]);
+/// let r = a.lt(&b);
+/// assert_eq!(r.array(), [false, false, true, false]);
+/// ```
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([-2, -1, 0, 1, 2]);
+/// let r = a.scalar_lt(1);
+/// assert_eq!(r.array(), [true, true, true, false, false]);
+/// ```
 pub fn lt<S: Shape, E: Unit, D: CmpKernel<LtKernelOp, E>, T: Tape<D>>(
     lhs: &Tensor<S, E, D, T>,
     rhs: &Tensor<S, E, D, T>,
@@ -89,7 +174,24 @@ pub fn lt<S: Shape, E: Unit, D: CmpKernel<LtKernelOp, E>, T: Tape<D>>(
     lhs.lt(rhs)
 }
 
+/// Element-wise less than or equals comparison. `<=`
 ///
+/// Examples:
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([1.2, 3.4, -0.5, -1.0]);
+/// let b = dev.tensor([1.2, 0.0, 3.14, -6.7]);
+/// let r = a.le(&b);
+/// assert_eq!(r.array(), [true, false, true, false]);
+/// ```
+/// ```
+/// # use dfdx::prelude::*;
+/// # let dev: Cpu = Default::default();
+/// let a = dev.tensor([-2, -1, 0, 1, 2]);
+/// let r = a.scalar_le(1);
+/// assert_eq!(r.array(), [true, true, true, true, false]);
+/// ```
 pub fn le<S: Shape, E: Unit, D: CmpKernel<LeKernelOp, E>, T: Tape<D>>(
     lhs: &Tensor<S, E, D, T>,
     rhs: &Tensor<S, E, D, T>,
@@ -99,18 +201,21 @@ pub fn le<S: Shape, E: Unit, D: CmpKernel<LeKernelOp, E>, T: Tape<D>>(
 
 // Macro to reduce boilerplate of implementing comparison methods on Tensor.
 macro_rules! impl_cmp_kernel_op {
-    ($kernel_op:ty, $try_op:ident, $op:ident, $try_scalar_op:ident, $scalar_op:ident) => {
+    ($kernel_op:ty, $try_op:ident, $op:ident, $try_scalar_op:ident, $scalar_op:ident, $doc:expr) => {
         impl<S: Shape, E: Unit, D: CmpKernel<$kernel_op, E>, T: Tape<D>> Tensor<S, E, D, T> {
+            #[doc = $doc]
             pub fn $try_op(&self, other: &Self) -> Result<Tensor<S, bool, D, NoneTape>, D::Err> {
                 try_cmp_op(self, other)
             }
 
+            #[doc = $doc]
             pub fn $op(&self, other: &Self) -> Tensor<S, bool, D, NoneTape> {
                 self.$try_op(other).unwrap()
             }
         }
 
         impl<S: Shape, E: Unit, D: ScalarCmpKernel<$kernel_op, E>, T: Tape<D>> Tensor<S, E, D, T> {
+            #[doc = $doc]
             pub fn $try_scalar_op(
                 &self,
                 scalar: E,
@@ -118,6 +223,7 @@ macro_rules! impl_cmp_kernel_op {
                 try_scalar_cmp_op(self, scalar)
             }
 
+            #[doc = $doc]
             pub fn $scalar_op(&self, scalar: E) -> Tensor<S, bool, D, NoneTape> {
                 self.$try_scalar_op(scalar).unwrap()
             }
@@ -125,12 +231,12 @@ macro_rules! impl_cmp_kernel_op {
     };
 }
 
-impl_cmp_kernel_op!(EqKernelOp, try_eq, eq, try_scalar_eq, scalar_eq);
-impl_cmp_kernel_op!(NeKernelOp, try_ne, ne, try_scalar_ne, scalar_ne);
-impl_cmp_kernel_op!(GtKernelOp, try_gt, gt, try_scalar_gt, scalar_gt);
-impl_cmp_kernel_op!(GeKernelOp, try_ge, ge, try_scalar_ge, scalar_ge);
-impl_cmp_kernel_op!(LtKernelOp, try_lt, lt, try_scalar_lt, scalar_lt);
-impl_cmp_kernel_op!(LeKernelOp, try_le, le, try_scalar_le, scalar_le);
+impl_cmp_kernel_op!(EqKernelOp, try_eq, eq, try_scalar_eq, scalar_eq, "See [eq]");
+impl_cmp_kernel_op!(NeKernelOp, try_ne, ne, try_scalar_ne, scalar_ne, "See [ne]");
+impl_cmp_kernel_op!(GtKernelOp, try_gt, gt, try_scalar_gt, scalar_gt, "See [gt]");
+impl_cmp_kernel_op!(GeKernelOp, try_ge, ge, try_scalar_ge, scalar_ge, "See [ge]");
+impl_cmp_kernel_op!(LtKernelOp, try_lt, lt, try_scalar_lt, scalar_lt, "See [lt]");
+impl_cmp_kernel_op!(LeKernelOp, try_le, le, try_scalar_le, scalar_le, "See [le]");
 
 #[cfg(test)]
 mod tests {
