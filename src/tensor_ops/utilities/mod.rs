3ad57ea8dc29d5d46f45bd62f0e04c630789ad1f
Nathan Koppel <nathankoppel0@gmail.com>
2023-02-14 19:29:27 -0600

    fix #451; rename internal_reshapes to reduction_utils

--- src/tensor_ops/utilities/mod.rs
+++ src/tensor_ops/utilities/mod.rs
@@ -3,7 +3,7 @@ pub(crate) mod cpu_kernels;
 #[cfg(feature = "cuda")]
 pub(crate) mod cuda_kernels;
 mod device;
-pub(crate) mod internal_reshapes;
+pub(crate) mod reduction_utils;
 pub(crate) mod ops;
 
 pub use backward::Backward;
