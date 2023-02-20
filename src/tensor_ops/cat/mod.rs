8c8c82fe6810a6db19ebb14d90e9ca261abc2b7d
Adithya Dsilva <adithyadsilva@icloud.com>
2023-02-20 13:03:51 +0530

    Fix `TryConcat` structure.

--- src/tensor_ops/cat/mod.rs
+++ src/tensor_ops/cat/mod.rs
@@ -1,14 +1,35 @@
-use crate::{shapes::*, tensor::*};
+use crate::{
+    gradients::{Merge, Tape},
+    shapes::*,
+    tensor::*,
+};
 
 /// Concatenate two tensors along the first dimension.
-pub trait TryConcat<Rhs: Shape>: DeviceStorage {
+pub trait TryConcat<E: Dtype>: DeviceStorage {
     /// Concatenate two tensors along the first dimension.
     ///
     /// TODO
-    fn concat(&self, rhs: &Rhs);
+    fn concat<A: Shape, B: Shape, T>(
+        &self,
+        a: Tensor<A, E, Self, T>,
+        b: Tensor<B, E, Self, T>,
+    ) -> Tensor<A::Extended, E, Self, T>
+    where
+        A: ExtendDim<B>,
+        T: Tape<Self> + Merge<T>,
+    {
+        self.try_concat(a, b).unwrap()
+    }
 
     /// Fallible version of [TryConcat::concat].
-    fn try_concat(&self, rhs: &Rhs) -> Result<(), Self::Err>;
+    fn try_concat<A: Shape, B: Shape, T>(
+        &self,
+        a: Tensor<A, E, Self, T>,
+        b: Tensor<B, E, Self, T>,
+    ) -> Result<Tensor<A::Extended, E, Self, T>, Self::Err>
+    where
+        A: ExtendDim<B>,
+        T: Tape<Self> + Merge<T>;
 }
 
 pub trait ExtendDim<Rhs: Shape>: Shape {
