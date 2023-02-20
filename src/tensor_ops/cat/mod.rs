04a3934bf4926a0e59dcad193cb91ac6544a28e1
Adithya Dsilva <adithyadsilva@icloud.com>
2023-02-20 21:15:25 +0530

    WIP

--- src/tensor_ops/cat/mod.rs
+++ src/tensor_ops/cat/mod.rs
@@ -37,22 +37,80 @@ pub trait ExtendDim<Rhs: Shape>: Shape {
     fn extend(&self, rhs: &Rhs) -> Self::Extended;
 }
 
-macro_rules! extend {
-    ($d1:ident, $($dn:ident),*) => {
-        impl<$d1: Dim, $($dn: Dim),*> ExtendDim<($d1, $($dn),*)> for (usize, $($dn),*) {
-            type Extended = (usize, $($dn),*);
-
-            fn extend(&self, rhs: &($d1, $($dn),*)) -> Self::Extended {
-                let mut shape = *self.shape();
-                shape.0 += rhs.0.size();
-                shape
-            }
-        }
-    };
+#[rustfmt::skip]
+impl<D: Dim, D1: Dim>
+    ExtendDim<(D1,)> for (D,)
+{
+    type Extended = (usize,);
+    fn extend(&self, rhs: &(D1,)) -> Self::Extended {
+        let s = self.shape();
+        (s.0.size() + rhs.0.size(),)
+    }
 }
-extend!(D1,);
-extend!(D1, D2);
-extend!(D1, D2, D3);
-extend!(D1, D2, D3, D4);
-extend!(D1, D2, D3, D4, D5);
-extend!(D1, D2, D3, D4, D5, D6);
+#[rustfmt::skip]
+impl<D: Dim, D1: Dim, D2: Dim>
+    ExtendDim<(D1, D2)> for (D, D2)
+{
+    type Extended = (usize, D2);
+    fn extend(&self, rhs: &(D1, D2)) -> Self::Extended {
+        let s = self.shape();
+        (s.0.size() + rhs.0.size(), s.1)
+    }
+}
+#[rustfmt::skip]
+impl<D: Dim, D1: Dim, D2: Dim, D3: Dim>
+    ExtendDim<(D1, D2, D3)> for (D, D2, D3)
+{
+    type Extended = (usize, D2, D3);
+    fn extend(&self, rhs: &(D1, D2, D3)) -> Self::Extended {
+        let s = self.shape();
+        (s.0.size() + rhs.0.size(), s.1, s.2)
+    }
+}
+#[rustfmt::skip]
+impl<D: Dim, D1: Dim, D2: Dim, D3: Dim, D4: Dim>
+    ExtendDim<(D1, D2, D3, D4)> for (D, D2, D3, D4)
+{
+    type Extended = (usize, D2, D3, D4);
+    fn extend(&self, rhs: &(D1, D2, D3, D4)) -> Self::Extended {
+        let s = self.shape();
+        (s.0.size() + rhs.0.size(), s.1, s.2, s.3)
+    }
+}
+#[rustfmt::skip]
+impl<D: Dim, D1: Dim, D2: Dim, D3: Dim, D4: Dim, D5: Dim>
+    ExtendDim<(D1, D2, D3, D4, D5)> for (D, D2, D3, D4, D5)
+{
+    type Extended = (usize, D2, D3, D4, D5);
+    fn extend(&self, rhs: &(D1, D2, D3, D4, D5)) -> Self::Extended {
+        let s = self.shape();
+        (s.0.size() + rhs.0.size(), s.1, s.2, s.3, s.4)
+    }
+}
+#[rustfmt::skip]
+impl<D: Dim, D1: Dim, D2: Dim, D3: Dim, D4: Dim, D5: Dim, D6: Dim>
+    ExtendDim<(D1, D2, D3, D4, D5, D6)> for (D, D2, D3, D4, D5, D6)
+{
+    type Extended = (usize, D2, D3, D4, D5, D6);
+    fn extend(&self, rhs: &(D1, D2, D3, D4, D5, D6)) -> Self::Extended {
+        let s = self.shape();
+        (s.0.size() + rhs.0.size(), s.1, s.2, s.3, s.4, s.5)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::tests::*;
+
+    #[test]
+    fn test_concat() {
+        let dev: TestDevice = Default::default();
+
+        let a: Tensor<(usize, Const<2>), TestDtype, _> = dev.zeros_like(&(3, Const));
+        let b: Tensor<(Const<5>, Const<2>), TestDtype, _> = dev.zeros_like(&(Const, Const));
+
+        a.shape().extend(b.shape());
+        todo!()
+    }
+}
