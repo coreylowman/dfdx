b4583acd080d577dc8a1adc8fa5874505fdea7c5
TimerErTim <timerertim@gmail.com>
2023-02-28 23:43:57 +0100

    Add npz save to examples

--- examples/numpy-save-load.rs
+++ examples/numpy-save-load.rs
@@ -5,6 +5,7 @@ fn main() {
     use dfdx::{
         shapes::{Rank0, Rank1, Rank2},
         tensor::{AsArray, Tensor, TensorFrom, ZerosTensor},
+        nn::{SaveToNpz, LoadFromNpz}
     };
 
     #[cfg(not(feature = "cuda"))]
@@ -27,6 +28,10 @@ fn main() {
         .save_to_npy("2d-rs.npy")
         .expect("Saving failed");
 
+    dev.tensor([3.0f32, 2.0, 1.0])
+        .save("1d-rs.npz")
+        .expect("Saving failed");
+
     let mut a: Tensor<Rank0, f32, _> = dev.zeros();
     a.load_from_npy("0d-rs.npy").expect("Loading failed");
     assert_eq!(a.array(), 1.234);
@@ -38,6 +43,10 @@ fn main() {
     let mut c: Tensor<Rank2<2, 3>, f32, _> = dev.zeros();
     c.load_from_npy("2d-rs.npy").expect("Loading failed");
     assert_eq!(c.array(), [[1.0, 2.0, 3.0], [-1.0, -2.0, -3.0]]);
+
+    let mut d: Tensor<Rank1<3>, f32, _> = dev.zeros();
+    d.load("1d-rs.npz").expect("Loading failed");
+    assert_eq!(d.array(), [3.0, 2.0, 1.0]);
 
     println!("Tensors were stored and loaded successfully");
 }
