6de4f88aeea1653b9071184e66726ba491ab39f8
Corey Lowman <clowman1993@gmail.com>
2023-03-03 12:02:36 -0500

    Style

--- src/tensor_ops/ema/ema.cu
+++ src/tensor_ops/ema/ema.cu
@@ -4,14 +4,13 @@ __device__ void ema(const size_t n, const T* src, T* dst, T decay) {
     if (i >= n) {
         return;
     }
-
     dst[i] = dst[i] * decay + src[i] * (1 - decay);
 }
 
 extern "C" __global__ void ema_f32(const size_t n, const float* src, float* dst, float decay) {
-    ema(n, src, dst ,decay);
+    ema(n, src, dst, decay);
 }
 
 extern "C" __global__ void ema_f64(const size_t n, const double* src, double* dst, double decay) {
-    ema(n, src, dst ,decay);
+    ema(n, src, dst, decay);
 }
