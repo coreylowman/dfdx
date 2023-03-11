f7f3bc1a4f2a198930112752a230ad4eb56bf6ae
Nathan Koppel <nathankoppel0@gmail.com>
2023-03-11 16:27:59 -0600

    add build_module.rs

---
+++ src/nn/build_module.rs
@@ -0,0 +1,43 @@
+use super::tensor_collection::*;
+use crate::prelude::{Device, DeviceStorage, Dtype, Shape, Tensor};
+
+struct Builder<'a, D: DeviceStorage>(&'a D);
+impl<'a, E: Dtype, D: Device<E>> TensorFunction<E, D, E, D> for Builder<'a, D> {
+    type Viewer = ();
+    type Err = D::Err;
+
+    fn apply<S: Shape>(
+        &mut self,
+        opts: TensorOptions<S, E, D>,
+        _t: (),
+    ) -> TensorFunctionOutput<Self, S, E, D, E, D> {
+        let mut tensor: Tensor<S, E, D> = self.0.try_zeros_like(&opts.shape)?;
+        (opts.reset)(&mut tensor)?;
+        Ok(Some(tensor))
+    }
+}
+
+/// Something that can be built. Related to [BuildOnDevice]
+pub trait BuildModule<D: Device<E>, E: Dtype>:
+    Sized + TensorCollection<E, D, Output<E, D> = Self>
+{
+    /// Construct it on the device
+    fn build(device: &D) -> Self {
+        Self::try_build(device).unwrap()
+    }
+
+    /// Fallible version of [BuildModule::build]
+    fn try_build(device: &D) -> Result<Self, D::Err> {
+        let out = Self::iter_tensors(&mut RecursiveWalker {
+            m: (),
+            f: &mut Builder(device),
+        })?;
+
+        Ok(out.unwrap())
+    }
+}
+
+impl<D: Device<E>, E: Dtype, M: Sized + TensorCollection<E, D, Output<E, D> = Self>>
+    BuildModule<D, E> for M
+{
+}
