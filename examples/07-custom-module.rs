dc4c281392fc63b0db6100816fe9382917eaf517
Nathan Koppel <nathankoppel0@gmail.com>
2023-03-12 17:09:20 -0500

    fix 07-custom-module

--- examples/07-custom-module.rs
+++ examples/07-custom-module.rs
@@ -1,11 +1,10 @@
-// TODO: fix this
 //! Demonstrates how to build a custom [nn::Module] without using tuples
 
 use dfdx::{
     nn::modules::{Linear, Module, ModuleVisitor, ModuleVisitorOutput, ReLU, TensorCollection},
     prelude::BuildModule,
     shapes::{Dtype, Rank1, Rank2},
-    tensor::{HasErr, SampleTensor, Tape, Tensor},
+    tensor::{SampleTensor, Tape, Tensor},
     tensor_ops::Device,
 };
 
@@ -15,47 +14,51 @@ type Dev = dfdx::tensor::Cpu;
 #[cfg(feature = "cuda")]
 type Dev = dfdx::tensor::Cuda;
 
-type Err = <Dev as HasErr>::Err;
-
 /// Custom model struct
 /// This case is trivial and should be done with a tuple of linears and relus,
 /// but it demonstrates how to build models with custom behavior
-struct Mlp<const IN: usize, const INNER: usize, const OUT: usize> {
-    l1: Linear<IN, INNER, f32, Dev>,
-    l2: Linear<INNER, OUT, f32, Dev>,
+struct Mlp<const IN: usize, const INNER: usize, const OUT: usize, E: Dtype, D: Device<E>> {
+    l1: Linear<IN, INNER, E, D>,
+    l2: Linear<INNER, OUT, E, D>,
     relu: ReLU,
 }
 
 // TensorCollection lets you do several operations on Modules, including constructing them with
-// randomized paramters
-impl<const IN: usize, const INNER: usize, const OUT: usize> TensorCollection<f32, Dev>
-    for Mlp<IN, INNER, OUT>
+// randomized parameters, and iterating through or mutating all tensors in a model.
+impl<const IN: usize, const INNER: usize, const OUT: usize, E: Dtype, D: Device<E>>
+    TensorCollection<E, D> for Mlp<IN, INNER, OUT, E, D>
 {
-    type Output<E2: Dtype, D2: Device<E2>> = Mlp<IN, INNER, OUT>;
+    // Type alias that specifies the how Mlp's type changes when using a different dtype and/or
+    // device.
+    type Output<E2: Dtype, D2: Device<E2>> = Mlp<IN, INNER, OUT, E2, D2>;
 
-    fn iter_tensors<E2: Dtype, D2: Device<E2>, V: ModuleVisitor<Self, f32, Dev, E2, D2>>(
+    fn iter_tensors<E2: Dtype, D2: Device<E2>, V: ModuleVisitor<Self, E, D, E2, D2>>(
         visitor: &mut V,
-    ) -> ModuleVisitorOutput<V::Func, Self, f32, Dev, E2, D2> {
-        // let l1 = visitor.visit_module("l1", |s| &s.l1, |s| &mut s.l1)?;
-        // let l2 = visitor.visit_module("l2", |s| &s.l2, |s| &mut s.l2)?;
+    ) -> ModuleVisitorOutput<V::Func, Self, E, D, E2, D2> {
+        // Specify the name of each field and how to access it immutably and mutably.
+        //
+        // Note that 'l1' and 'l2' are of type 'Option<Linear<...>>', and that these options should
+        // not be handled until we construct the output Mlp, because a value of None is not an error.
+        let l1 = visitor.visit_module("l1", |s| &s.l1, |s| &mut s.l1)?;
+        let l2 = visitor.visit_module("l2", |s| &s.l2, |s| &mut s.l2)?;
 
-        // Ok(dfdx::try_some!(Mlp {
-        // l1: l1?,
-        // l2: l2?,
-        // relu: Default::default(),
-        // }))
-        todo!()
+        // Specify how to construct Mlp given optional values for its fields. Note that
+        Ok(dfdx::try_some!(Mlp {
+            l1: l1?,
+            l2: l2?,
+            relu: Default::default(),
+        }))
     }
 }
 
 // impl Module for single item
-impl<const IN: usize, const INNER: usize, const OUT: usize> Module<Tensor<Rank1<IN>, f32, Dev>>
-    for Mlp<IN, INNER, OUT>
+impl<const IN: usize, const INNER: usize, const OUT: usize, E: Dtype, D: Device<E>>
+    Module<Tensor<Rank1<IN>, E, D>> for Mlp<IN, INNER, OUT, E, D>
 {
-    type Output = Tensor<Rank1<OUT>, f32, Dev>;
-    type Error = Err;
+    type Output = Tensor<Rank1<OUT>, E, D>;
+    type Error = D::Err;
 
-    fn try_forward(&self, x: Tensor<Rank1<IN>, f32, Dev>) -> Result<Self::Output, Err> {
+    fn try_forward(&self, x: Tensor<Rank1<IN>, E, D>) -> Result<Self::Output, D::Err> {
         let x = self.l1.try_forward(x)?;
         let x = self.relu.try_forward(x)?;
         self.l2.try_forward(x)
@@ -68,13 +71,15 @@ impl<
         const IN: usize,
         const INNER: usize,
         const OUT: usize,
-        T: Tape<f32, Dev>,
-    > Module<Tensor<Rank2<BATCH, IN>, f32, Dev, T>> for Mlp<IN, INNER, OUT>
+        E: Dtype,
+        D: Device<E>,
+        T: Tape<E, D>,
+    > Module<Tensor<Rank2<BATCH, IN>, E, D, T>> for Mlp<IN, INNER, OUT, E, D>
 {
-    type Output = Tensor<Rank2<BATCH, OUT>, f32, Dev, T>;
-    type Error = Err;
+    type Output = Tensor<Rank2<BATCH, OUT>, E, D, T>;
+    type Error = D::Err;
 
-    fn try_forward(&self, x: Tensor<Rank2<BATCH, IN>, f32, Dev, T>) -> Result<Self::Output, Err> {
+    fn try_forward(&self, x: Tensor<Rank2<BATCH, IN>, E, D, T>) -> Result<Self::Output, D::Err> {
         let x = self.l1.try_forward(x)?;
         let x = self.relu.try_forward(x)?;
         self.l2.try_forward(x)
@@ -86,7 +91,7 @@ fn main() {
     let dev = Dev::default();
 
     // Construct model
-    let model = Mlp::<10, 512, 20>::build(&dev);
+    let model = Mlp::<10, 512, 20, f32, Dev>::build(&dev);
 
     // Forward pass with a single sample
     let item: Tensor<Rank1<10>, f32, _> = dev.sample_normal();
