4695bc8e2ad6559af80e6ffde5eb6997540d3209
Corey Lowman <clowman1993@gmail.com>
2023-03-25 17:00:22 +0000

    max_to

--- src/tensor_ops/max_to/cuda_kernel.rs
+++ src/tensor_ops/max_to/cuda_kernel.rs
@@ -4,7 +4,7 @@ use crate::{
     tensor_ops::reduction_utils::*,
 };
 
-use cudarc::driver::{CudaSlice, DeviceSlice, LaunchAsync};
+use cudarc::driver::{DeviceSlice, LaunchAsync};
 
 const PTX_SRC: &str = include_str!(concat!(env!("OUT_DIR"), "/max_to.ptx"));
 
@@ -43,34 +43,36 @@ where
         }
 
         let fill_fn = self.dev.get_func(Self::MOD, Self::FNS[2]).unwrap();
-        let mut storage = unsafe {
-            let mut storage = self.dev.alloc::<E>(dst.num_elements())?;
-            fill_fn.launch(
-                launch_cfg(dst.num_elements() as u32),
-                (&mut storage, Self::INIT, dst.num_elements()),
-            )?;
-            storage
-        };
-
         let fwd_fn = self.dev.get_func(Self::MOD, Self::FNS[0]).unwrap();
-
-        let (dims, strides) = permute_for_reductions::<_, Ax>(inp.shape.concrete(), inp.strides);
-        let dims: CudaSlice<usize> = self.dev.htod_copy(dims)?;
-        let strides: CudaSlice<usize> = self.dev.htod_copy(strides)?;
 
         let physical_numel = inp.data.len();
         let (dst_physical_numel, dst_strides) =
             reduction_output_strides::<Ax, Src, Dst>(inp.strides, dst);
         let chunk_len = physical_numel / dst_physical_numel;
 
+        let (dims, strides) = permute_for_reductions::<_, Ax>(inp.shape.concrete(), inp.strides);
+        let num_dims = dims.len();
+        let mut info = Vec::with_capacity(dims.len() * 2);
+        info.extend(dims);
+        info.extend(strides);
+
         let cfg = launch_cfg(physical_numel as u32);
+
+        let info = self.dev.htod_copy(info)?;
+        let mut storage = unsafe { self.dev.alloc::<E>(dst.num_elements()) }?;
+        unsafe {
+            fill_fn.launch(
+                launch_cfg(dst.num_elements() as u32),
+                (&mut storage, Self::INIT, dst.num_elements()),
+            )
+        }?;
+
         let params = (
-            physical_numel,    // const size_t numel,
-            dims.len(),        // const size_t num_dims,
-            chunk_len,         // const size_t chunk_len,
+            physical_numel, // const size_t numel,
+            num_dims,       // const size_t num_dims,
+            chunk_len,      // const size_t chunk_len,
+            &info,
             inp.data.as_ref(), // const float *inp,
-            &dims,             // const size_t *dims,
-            &strides,          // const size_t *strides,
             &mut storage,      // float *out
         );
         unsafe { fwd_fn.launch(cfg, params) }?;
@@ -89,12 +91,8 @@ where
     {
         let bwd_fn = self.dev.get_func(Self::MOD, Self::FNS[1]).unwrap();
 
-        let dims: CudaSlice<usize> = self.dev.htod_copy(inp.shape.concrete().into())?;
-        let inp_strides: CudaSlice<usize> = self.dev.htod_copy(inp.strides.into())?;
         let out_strides: Src::Concrete =
             BroadcastStridesTo::<Src, Ax>::broadcast_strides(&out.shape, out.strides);
-        let out_strides: CudaSlice<usize> = self.dev.htod_copy(out_strides.into())?;
-
         let physical_numel = grad_inp.len();
         let elems_per_thread = E::from_usize(reduction_elems_per_thread::<_, Src>(
             inp.shape.concrete(),
@@ -104,17 +102,22 @@ where
         .unwrap();
 
         let cfg = launch_cfg(physical_numel as u32);
+        let mut info = Vec::with_capacity(Src::NUM_DIMS * 3);
+        info.extend(inp.shape.concrete());
+        info.extend(inp.strides);
+        info.extend(out_strides);
+
+        let info = self.dev.htod_copy(info)?;
+
         let params = (
             physical_numel,    // const size_t numel,
             Src::NUM_DIMS,     // const size_t num_dims,
             elems_per_thread,  // const float elems_per_thread,
-            &dims,             // const size_t *dims,
+            &info,             // const size_t *dims,
             inp.data.as_ref(), // const float *inp,
             grad_inp,          // float *grad_inp,
-            &inp_strides,      // const size_t *inp_strides,
             out.data.as_ref(), // const float *out,
             grad_out,          // const float *grad_out,
-            &out_strides,      // const size_t *out_strides
         );
         unsafe { bwd_fn.launch(cfg, params) }?;
         Ok(())
