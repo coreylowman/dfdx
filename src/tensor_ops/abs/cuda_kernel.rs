f65fe6aefb7b0da8901b5be691942f99f45dbcf6
Viliam Vadocz <viliam.vadocz@gmail.com>
2023-04-12 22:45:51 +0200

    Begin working on f16 cuda kernels

--- src/tensor_ops/abs/cuda_kernel.rs
+++ src/tensor_ops/abs/cuda_kernel.rs
@@ -1,9 +1,16 @@
 use super::AbsKernelOp;
 use crate::tensor_ops::cuda_kernels::cuda_unary;
 
+#[cfg(feature = "f16")]
+use half::{bf16, f16};
+
 unsafe impl cudarc::driver::DeviceRepr for AbsKernelOp {}
 
 const PTX: &str = include_str!(concat!(env!("OUT_DIR"), "/abs.ptx"));
 
+#[cfg(feature = "f16")]
+cuda_unary!(AbsKernelOp, f16, PTX, "abs_fwd_f16", "abs_bwd_f16");
+// #[cfg(feature = "f16")]
+// cuda_unary!(AbsKernelOp, bf16, PTX, "abs_fwd_bf16", "abs_bwd_bf16");
 cuda_unary!(AbsKernelOp, f32, PTX, "abs_fwd_f32", "abs_bwd_f32");
 cuda_unary!(AbsKernelOp, f64, PTX, "abs_fwd_f64", "abs_bwd_f64");
