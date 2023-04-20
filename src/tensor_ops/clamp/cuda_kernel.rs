b571d6602397b9be59d408ec3ce8a5da7bee8a0e
Corey Lowman <clowman1993@gmail.com>
2023-04-20 14:57:15 -0400

    Formatting

--- src/tensor_ops/clamp/cuda_kernel.rs
+++ src/tensor_ops/clamp/cuda_kernel.rs
@@ -9,6 +9,12 @@ unsafe impl cudarc::driver::DeviceRepr for ClampKernelOp<f64> {}
 const P: &str = include_str!(concat!(env!("OUT_DIR"), "/clamp.ptx"));
 
 #[cfg(feature = "f16")]
-cuda_unary!(ClampKernelOp<half::f16>, half::f16, P, "clamp_fwd_f16", "clamp_bwd_f16");
+cuda_unary!(
+    ClampKernelOp<half::f16>,
+    half::f16,
+    P,
+    "clamp_fwd_f16",
+    "clamp_bwd_f16"
+);
 cuda_unary!(ClampKernelOp<f32>, f32, P, "clamp_fwd_f32", "clamp_bwd_f32");
 cuda_unary!(ClampKernelOp<f64>, f64, P, "clamp_fwd_f64", "clamp_bwd_f64");
--- src/tensor_ops/sin/cuda_kernel.rs
+++ src/tensor_ops/sin/cuda_kernel.rs
@@ -5,6 +5,12 @@ unsafe impl cudarc::driver::DeviceRepr for super::SinKernelOp {}
 const PTX: &str = include_str!(concat!(env!("OUT_DIR"), "/sin.ptx"));
 
 #[cfg(feature = "f16")]
-cuda_unary!(super::SinKernelOp, half::f16, PTX, "sin_fwd_f16", "sin_bwd_f16");
+cuda_unary!(
+    super::SinKernelOp,
+    half::f16,
+    PTX,
+    "sin_fwd_f16",
+    "sin_bwd_f16"
+);
 cuda_unary!(super::SinKernelOp, f32, PTX, "sin_fwd_f32", "sin_bwd_f32");
 cuda_unary!(super::SinKernelOp, f64, PTX, "sin_fwd_f64", "sin_bwd_f64");
