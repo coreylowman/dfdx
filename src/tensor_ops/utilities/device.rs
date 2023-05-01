08ccd1b204e8a903de4161b92bc4c9fdda0cc8b1
Corey Lowman <clowman1993@gmail.com>
2023-05-01 15:32:15 -0400

    Adds CmpKernel bounds to Device

--- src/tensor_ops/utilities/device.rs
+++ src/tensor_ops/utilities/device.rs
@@ -62,6 +62,12 @@ pub trait Device<E: Dtype>:
 
     // boolean operations
     + super::super::boolean::BooleanKernel
+    + super::super::cmp::CmpKernel<super::super::cmp::EqKernelOp, E>
+    + super::super::cmp::CmpKernel<super::super::cmp::NeKernelOp, E>
+    + super::super::cmp::CmpKernel<super::super::cmp::GtKernelOp, E>
+    + super::super::cmp::CmpKernel<super::super::cmp::GeKernelOp, E>
+    + super::super::cmp::CmpKernel<super::super::cmp::LtKernelOp, E>
+    + super::super::cmp::CmpKernel<super::super::cmp::LeKernelOp, E>
     + super::super::cmp::ScalarCmpKernel<super::super::cmp::EqKernelOp, E>
     + super::super::cmp::ScalarCmpKernel<super::super::cmp::NeKernelOp, E>
     + super::super::cmp::ScalarCmpKernel<super::super::cmp::GtKernelOp, E>
