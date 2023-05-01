fff5bd50ae8b7d9fff1cb40cd79793f9bf7d2c5c
Corey Lowman <clowman1993@gmail.com>
2023-05-01 16:17:08 -0400

    Using Bernoulli distribution in dropout

--- src/tensor_ops/dropout/mod.rs
+++ src/tensor_ops/dropout/mod.rs
@@ -10,20 +10,20 @@ use crate::{
 
 #[repr(C)]
 #[derive(Debug, Clone, Copy)]
-pub struct DropoutKernelOp<F> {
+pub struct DropoutKernelOp {
     pub seed: u64,
-    pub prob: F,
+    pub prob: f64,
 }
 
 pub trait DropoutKernel<E: Dtype>: DeviceStorage {
     fn forward<S: Shape>(
         &self,
-        op: DropoutKernelOp<E>,
+        op: DropoutKernelOp,
         inp: &Tensor<S, E, Self>,
     ) -> Result<Tensor<S, E, Self>, Self::Err>;
     fn backward<S: Shape>(
         &self,
-        op: DropoutKernelOp<E>,
+        op: DropoutKernelOp,
         inp: &Tensor<S, E, Self>,
         grad_inp: &mut Self::Vec<E>,
         grad_out: &Self::Vec<E>,
@@ -64,7 +64,7 @@ impl<S: Shape, E: Dtype, D: DropoutKernel<E>, T: Tape<E, D>> Tensor<S, E, D, T> 
     /// See [dropout]
     pub fn try_dropout(self, prob: impl Into<f64>) -> Result<Self, D::Err> {
         let seed = self.device.random_u64();
-        let prob = E::from_f64(prob.into()).unwrap();
+        let prob = prob.into();
         let op = DropoutKernelOp { seed, prob };
         let (inp, mut tape) = self.split_tape();
         let out = inp.device.forward(op, &inp)?;
@@ -87,44 +87,48 @@ mod tests {
     #[test]
     fn test_dropout_all_0d() {
         let dev: TestDevice = Default::default();
-        let t: Tensor<_, f32, _> = dev.tensor(3.0);
+        let t = dev.tensor([3.0, 4.0, 5.0]).to_dtype::<TestDtype>();
         let r = t.leaky_trace().dropout(1.0);
-        assert_close_to_literal!(r, 0.0);
-        let g = r.backward();
-        assert_close_to_literal!(g.get(&t), 0.0);
+        assert_close_to_literal!(r, [0.0, 0.0, 0.0]);
+        let g = r.sum().backward();
+        assert_close_to_literal!(g.get(&t), [0.0, 0.0, 0.0]);
     }
 
     #[test]
     fn test_dropout_none_0d() {
         let dev: TestDevice = Default::default();
-        let t: Tensor<_, f32, _> = dev.tensor(3.0);
+        let t = dev.tensor([3.0, 4.0, 5.0]).to_dtype::<TestDtype>();
         let r = t.leaky_trace().dropout(0.0);
-        assert_close_to_literal!(r, 3.0);
-        let g = r.backward();
-        assert_close_to_literal!(g.get(&t), 1.0);
+        assert_close_to_literal!(r, [3.0, 4.0, 5.0]);
+        let g = r.sum().backward();
+        assert_close_to_literal!(g.get(&t), [1.0, 1.0, 1.0]);
     }
 
     #[test]
     fn test_dropout_1d_with_non_positive_values() {
-        let dev: TestDevice = Default::default();
-        let t: Tensor<_, f32, _> = dev.tensor([0.0, 2.0, -3.0, -4.0, 0.0]);
+        let dev: TestDevice = TestDevice::seed_from_u64(1);
+        let t = dev
+            .tensor([-0.0, 2.0, -3.0, -4.0, -0.0])
+            .to_dtype::<TestDtype>();
         let r = t.leaky_trace().dropout(0.5);
-        assert_close_to_literal!(r, [0.0, 4.0, -6.0, 0.0, 0.0]);
+        assert_close_to_literal!(r, [0.0, 4.0, -6.0, -8.0, 0.0]);
         let g = r.mean().backward();
-        assert_close_to_literal!(g.get(&t), [0.4, 0.4, 0.4, 0.0, 0.0]);
+        assert_close_to_literal!(g.get(&t), [0.4, 0.4, 0.4, 0.4, 0.0]);
     }
 
     #[test]
     fn test_dropout_2d() {
         let dev: TestDevice = Default::default();
-        let t: Tensor<_, f32, _> = dev.tensor([[0.05, 0.1, -0.2], [0.3, -0.4, 0.5]]);
+        let t = dev
+            .tensor([[0.05, 0.1, -0.2], [0.3, -0.4, 0.5]])
+            .to_dtype::<TestDtype>();
         let r = t.leaky_trace().dropout(0.6);
-        assert_close_to_literal!(r, [[0.125, 0.25, -0.5], [0.0, 0.0, 1.25]]);
+        assert_close_to_literal!(r, [[0.125, 0.0, -0.5], [0.0, -1.0, 0.0]]);
         // NOTE: .exp() so we ensure result grad is used properly
         let g = r.exp().mean().backward();
         assert_close_to_literal!(
             g.get(&t),
-            [[0.47214523, 0.5350107, 0.2527211], [0.0, 0.0, 1.4543099]]
+            [[0.4721452, 0.0, 0.25272113], [0.0, 0.1532831, 0.0]]
         );
     }
 }
