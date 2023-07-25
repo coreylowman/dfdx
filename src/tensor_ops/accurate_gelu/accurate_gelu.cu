e7ebf8ef4b234d76b94270b13905ef112e204e8c
Jan Cristina <jan.cristina@gmail.com>
2023-07-25 21:00:31 +0300

    reformat

--- src/tensor_ops/accurate_gelu/accurate_gelu.cu
+++ src/tensor_ops/accurate_gelu/accurate_gelu.cu
@@ -5,29 +5,29 @@
 struct AccurateGeLUKernelOp {};
 
 template <typename T> __device__ T accurate_gelu_fwd(T x) {
-  T one = 1.0;
-  T half = 0.5;
-  T alpha = M_SQRT1_2;
-  return half * x * (one + erfg(x * alpha));
+    T one = 1.0;
+    T half = 0.5;
+    T alpha = M_SQRT1_2;
+    return half * x * (one + erfg(x * alpha));
 }
 
 template <typename T> __device__ T accurate_gelu_bwd(T x) {
-  T one = 1.0;
-  T half = 0.5;
-  T alpha = M_SQRT1_2;
-  T scale = M_2_SQRTPI;
-  T x_sq = x * x;
-  T arg = -half * x_sq;
-  T norm = scale * expg(arg);
+    T one = 1.0;
+    T half = 0.5;
+    T alpha = M_SQRT1_2;
+    T scale = M_2_SQRTPI;
+    T x_sq = x * x;
+    T arg = -half * x_sq;
+    T norm = scale * expg(arg);
 
-  T left = half * x;
-  T right = one + erfg(alpha * x);
+    T left = half * x;
+    T right = one + erfg(alpha * x);
 
-  T left_derivative = half * right;
+    T left_derivative = half * right;
 
-  T right_derivative = left * norm;
+    T right_derivative = left * norm;
 
-  return left_derivative + right_derivative;
+    return left_derivative + right_derivative;
 }
 
 UNARY_OP(__half, accurate_gelu_fwd_f16, accurate_gelu_bwd_f16,
