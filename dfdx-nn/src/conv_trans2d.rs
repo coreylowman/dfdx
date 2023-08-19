897b3191ac62978a9344e6afbe9c26ff1cae0d95
Corey Lowman <clowman1993@gmail.com>
2023-08-19 11:44:38 -0400

    Adding ConvTrans2D

---
+++ dfdx-nn/src/conv_trans2d.rs
@@ -0,0 +1,148 @@
+use dfdx::{
+    shapes::{Const, Dim, Dtype, HasShape},
+    tensor::Tensor,
+    tensor_ops::{Device, TryConvTrans2D},
+};
+
+use crate::*;
+
+#[derive(Debug, Default, Clone, Copy)]
+pub struct ConvTrans2DConfig<
+    InChan: Dim,
+    OutChan: Dim,
+    KernelSize: Dim,
+    Stride: Dim = Const<1>,
+    Padding: Dim = Const<0>,
+    Dilation: Dim = Const<1>,
+    Groups: Dim = Const<1>,
+> {
+    pub in_chan: InChan,
+    pub out_chan: OutChan,
+    pub kernel_size: KernelSize,
+    pub stride: Stride,
+    pub padding: Padding,
+    pub dilation: Dilation,
+    pub groups: Groups,
+}
+
+pub type ConvTrans2DConstConfig<
+    const IN_CHAN: usize,
+    const OUT_CHAN: usize,
+    const KERNEL_SIZE: usize,
+    const STRIDE: usize = 1,
+    const PADDING: usize = 0,
+    const DILATION: usize = 1,
+    const GROUPS: usize = 1,
+> = ConvTrans2DConfig<
+    Const<IN_CHAN>,
+    Const<OUT_CHAN>,
+    Const<KERNEL_SIZE>,
+    Const<STRIDE>,
+    Const<PADDING>,
+    Const<DILATION>,
+    Const<GROUPS>,
+>;
+
+impl<I: Dim, O: Dim, K: Dim, S: Dim, P: Dim, L: Dim, G: Dim, E: Dtype, D: Device<E>>
+    crate::BuildOnDevice<E, D> for ConvTrans2DConfig<I, O, K, S, P, L, G>
+where
+    O: std::ops::Div<G>,
+    <O as std::ops::Div<G>>::Output: Dim,
+{
+    type Built = ConvTrans2D<I, O, K, S, P, L, G, E, D>;
+    fn try_build_on_device(&self, device: &D) -> Result<Self::Built, <D>::Err> {
+        assert_eq!(self.in_chan.size() % self.groups.size(), 0);
+        assert_eq!(self.out_chan.size() % self.groups.size(), 0);
+        let o_over_g = self.out_chan / self.groups;
+        let weight =
+            device.try_zeros_like(&(self.in_chan, o_over_g, self.kernel_size, self.kernel_size))?;
+        Ok(ConvTrans2D {
+            weight,
+            stride: self.stride,
+            padding: self.padding,
+            dilation: self.dilation,
+            groups: self.groups,
+        })
+    }
+}
+
+#[derive(Debug, Clone, UpdateParams, ZeroGrads, SaveSafeTensors, LoadSafeTensors)]
+pub struct ConvTrans2D<InChan, OutChan, KernelSize, Stride, Padding, Dilation, Groups, Elem, Dev>
+where
+    OutChan: std::ops::Div<Groups>,
+    <OutChan as std::ops::Div<Groups>>::Output: Dim,
+    InChan: Dim,
+    OutChan: Dim,
+    KernelSize: Dim,
+    Stride: Dim,
+    Padding: Dim,
+    Dilation: Dim,
+    Groups: Dim,
+    Elem: Dtype,
+    Dev: Device<Elem>,
+{
+    #[param]
+    #[serialize]
+    pub weight: Tensor<
+        (
+            InChan,
+            <OutChan as std::ops::Div<Groups>>::Output,
+            KernelSize,
+            KernelSize,
+        ),
+        Elem,
+        Dev,
+    >,
+    pub stride: Stride,
+    pub padding: Padding,
+    pub dilation: Dilation,
+    pub groups: Groups,
+}
+
+impl<I: Dim, O: Dim, K: Dim, S: Dim, P: Dim, L: Dim, G: Dim, E, D> crate::ResetParams<E, D>
+    for ConvTrans2D<I, O, K, S, P, L, G, E, D>
+where
+    O: std::ops::Div<G>,
+    <O as std::ops::Div<G>>::Output: Dim,
+    E: Dtype + num_traits::Float + rand_distr::uniform::SampleUniform,
+    D: Device<E>,
+{
+    fn try_reset_params(&mut self) -> Result<(), D::Err> {
+        let (_, o_over_g, k, _) = self.weight.shape();
+        let scale = E::from_f64(1.0 / (k.size() * k.size() * o_over_g.size()) as f64).unwrap();
+        let b = scale.sqrt();
+        self.weight
+            .try_fill_with_distr(rand_distr::Uniform::new(-b, b))
+    }
+}
+
+impl<I: Dim, O: Dim, K: Dim, S: Dim, P: Dim, L: Dim, G: Dim, E, D, Img> crate::Module<Img>
+    for ConvTrans2D<I, O, K, S, P, L, G, E, D>
+where
+    O: std::ops::Div<G>,
+    <O as std::ops::Div<G>>::Output: Dim,
+    E: Dtype,
+    D: Device<E>,
+    (
+        Img,
+        Tensor<(I, <O as std::ops::Div<G>>::Output, K, K), E, D>,
+    ): TryConvTrans2D<S, P, L, G>,
+{
+    type Output = <(
+        Img,
+        Tensor<(I, <O as std::ops::Div<G>>::Output, K, K), E, D>,
+    ) as TryConvTrans2D<S, P, L, G>>::Convolved;
+    type Error = <(
+        Img,
+        Tensor<(I, <O as std::ops::Div<G>>::Output, K, K), E, D>,
+    ) as TryConvTrans2D<S, P, L, G>>::Error;
+
+    fn try_forward(&self, x: Img) -> Result<Self::Output, Self::Error> {
+        (x, self.weight.clone()).try_convtrans2d(
+            self.stride,
+            self.padding,
+            self.dilation,
+            self.groups,
+        )
+    }
+}
