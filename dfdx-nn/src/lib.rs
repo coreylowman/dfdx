a08349642f259b66d660f7f0b212182f872c2dc1
Corey Lowman <clowman1993@gmail.com>
2023-09-05 11:49:48 -0700

    Moe layers to dfdx-nn/src/layers

--- dfdx-nn/src/lib.rs
+++ dfdx-nn/src/lib.rs
@@ -1,114 +1,11 @@
 #![cfg_attr(feature = "nightly", feature(generic_const_exprs))]
 
+mod layers;
 pub mod optim;
-
-mod abs;
-mod add_into;
-mod batch_norm1d;
-mod batch_norm2d;
-mod bias1d;
-mod bias2d;
-#[cfg(feature = "nightly")]
-mod conv2d;
-#[cfg(feature = "nightly")]
-mod conv_trans2d;
-mod cos;
-mod dropout;
-mod embedding;
-mod exp;
-#[cfg(feature = "nightly")]
-mod flatten2d;
-mod gelu;
-mod generalized_add;
-mod generalized_mul;
-mod layer_norm1d;
-mod leaky_relu;
-mod linear;
-mod ln;
-mod log_softmax;
-mod matmul;
-mod multi_head_attention;
-#[cfg(feature = "nightly")]
-mod pool_2d_avg;
-#[cfg(feature = "nightly")]
-mod pool_2d_max;
-#[cfg(feature = "nightly")]
-mod pool_2d_min;
-mod pool_global_avg;
-mod pool_global_max;
-mod pool_global_min;
-mod prelu;
-mod prelu1d;
-mod relu;
-mod reshape;
-mod residual_add;
-mod residual_mul;
-mod sigmoid;
-mod sin;
-mod softmax;
-mod split_into;
-mod sqrt;
-mod square;
-mod tanh;
-mod transformer;
-mod upscale2d;
 
 pub use dfdx_nn_core::*;
 pub use dfdx_nn_derives::*;
-
-pub use abs::Abs;
-pub use add_into::AddInto;
-pub use batch_norm1d::{BatchNorm1D, BatchNorm1DConfig, BatchNorm1DConstConfig};
-pub use batch_norm2d::{BatchNorm2D, BatchNorm2DConfig, BatchNorm2DConstConfig};
-pub use bias1d::{Bias1D, Bias1DConfig, Bias1DConstConfig};
-pub use bias2d::{Bias2D, Bias2DConfig, Bias2DConstConfig};
-#[cfg(feature = "nightly")]
-pub use conv2d::{Conv2D, Conv2DConfig, Conv2DConstConfig};
-#[cfg(feature = "nightly")]
-pub use conv_trans2d::{ConvTrans2D, ConvTrans2DConfig, ConvTrans2DConstConfig};
-pub use cos::Cos;
-pub use dropout::{Dropout, DropoutOneIn};
-pub use embedding::{Embedding, EmbeddingConfig, EmbeddingConstConfig};
-pub use exp::Exp;
-#[cfg(feature = "nightly")]
-pub use flatten2d::Flatten2D;
-pub use gelu::{AccurateGeLU, FastGeLU};
-pub use generalized_add::GeneralizedAdd;
-pub use generalized_mul::GeneralizedMul;
-pub use layer_norm1d::{LayerNorm1D, LayerNorm1DConfig, LayerNorm1DConstConfig};
-pub use leaky_relu::LeakyReLU;
-pub use linear::{Linear, LinearConfig, LinearConstConfig};
-pub use ln::Ln;
-pub use log_softmax::LogSoftmax;
-pub use matmul::{MatMul, MatMulConfig, MatMulConstConfig};
-pub use multi_head_attention::{MultiHeadAttention, MultiHeadAttentionConfig};
-#[cfg(feature = "nightly")]
-pub use pool_2d_avg::{AvgPool2D, AvgPool2DConst};
-#[cfg(feature = "nightly")]
-pub use pool_2d_max::{MaxPool2D, MaxPool2DConst};
-#[cfg(feature = "nightly")]
-pub use pool_2d_min::{MinPool2D, MinPool2DConst};
-pub use pool_global_avg::AvgPoolGlobal;
-pub use pool_global_max::MaxPoolGlobal;
-pub use pool_global_min::MinPoolGlobal;
-pub use prelu::{PReLU, PReLUConfig};
-pub use prelu1d::{PReLU1D, PReLU1DConfig};
-pub use relu::ReLU;
-pub use reshape::Reshape;
-pub use residual_add::ResidualAdd;
-pub use residual_mul::ResidualMul;
-pub use sigmoid::Sigmoid;
-pub use sin::Sin;
-pub use softmax::Softmax;
-pub use split_into::SplitInto;
-pub use sqrt::Sqrt;
-pub use square::Square;
-pub use tanh::Tanh;
-pub use transformer::{
-    DecoderBlock, DecoderBlockConfig, EncoderBlock, EncoderBlockConfig, Transformer,
-    TransformerConfig,
-};
-pub use upscale2d::{Upscale2D, Upscale2DBy, Upscale2DByConst, Upscale2DConst};
+pub use layers::*;
 
 #[cfg(test)]
 pub(crate) mod tests {
