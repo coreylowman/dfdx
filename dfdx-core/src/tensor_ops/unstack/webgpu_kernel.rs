5f41a47ac56c11b8713e765ba4fb6ada40dd9f25
Thiago Machado <swfsql@gmail.com>
2024-02-06 18:35:44 -0500

    fix wgpu signature

--- dfdx-core/src/tensor_ops/unstack/webgpu_kernel.rs
+++ dfdx-core/src/tensor_ops/unstack/webgpu_kernel.rs
@@ -1,11 +1,14 @@
 use crate::{prelude::NoneTape, shapes::*, tensor::Webgpu};
 use std::vec::Vec;
 
-impl<E: Dtype, Items> super::UnstackKernel<E, Items> for Webgpu {
-    fn forward<S: Shape>(&self, stack: Tensor<S, E, Self, NoneTape>) -> Result<Items, Error>
+impl<E: Dtype> super::UnstackKernel<E> for Webgpu {
+    fn forward<Items, S: Shape, T: Tape<E, Self>>(
+        &self,
+        stack: Tensor<S, E, Self, NoneTape>,
+    ) -> Result<Items, Error>
     where
         S: super::SubDim,
-        Items: Array<Option<Tensor<S::Tail, E, Self, NoneTape>>, Dim = S::Head>,
+        Items: Array<Tensor<S::Tail, E, Self, T>, Dim = S::Head>,
     {
         todo!()
     }
